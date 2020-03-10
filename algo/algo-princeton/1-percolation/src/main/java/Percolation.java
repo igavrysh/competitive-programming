4f5d4a9f1d3fc1cd3bf32d87ebe29b03ba69dba8
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-03-10 07:34:57 -0700

    Added quick unoin and weighter quick union union find algos

--- algo/algo-princeton/1-percolation/src/main/java/Percolation.java
+++ algo/algo-princeton/1-percolation/src/main/java/Percolation.java
@@ -3,8 +3,16 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+
+    private int[] id;
+
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Grid size must be positive number");
+        }
+
+        id = new int[n];
     }
 
     // opens the site (row, col) if it is not open already
@@ -34,5 +42,73 @@ public class Percolation {
     // test client (optional)
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+    }
+}
+
+class QuickUnionUnionFind {
+    private int[] id;
+
+    public QuickUnionUnionFind(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+}
+
+class WeightedQuickUnionUnionFind {
+    private int[] id;
+    private int[] sz;
+
+    public WeightedQuickUnionUnionFind(int N) {
+        id = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (i == j) {
+            return;
+        }
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
     }
 }
