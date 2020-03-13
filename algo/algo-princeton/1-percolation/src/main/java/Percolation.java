71a566748be4e488504d79824e2b09bf69371b3a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-03-13 02:32:36 -0700

    Next version of percolations -- adding tests in progress

--- algo/algo-princeton/1-percolation/src/main/java/Percolation.java
+++ algo/algo-princeton/1-percolation/src/main/java/Percolation.java
@@ -4,39 +4,81 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private int[] id;
+    private WeightedQuickUnionUnionFind uf;
+    private int n;
+    private int openedQnt = 0;
+    private boolean[] opened;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("Grid size must be positive number");
         }
-
-        id = new int[n];
+        this.n = n;
+        uf = new WeightedQuickUnionUnionFind(n + 2);
+        opened = new boolean[n * n];
+        for (int i = 0; i < n; i++) {
+            uf.union(i, n);
+            uf.union(n * (n - 1) + i, n + 1);
+            for (int j = 0; j < n; j++) {
+                opened[i * n + j] = false;
+            }
+        }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        if (!isValid(row, col)) {
+            throw new IllegalArgumentException("Rows and cols must be within size of the grid");
+        }
+
+        if (!opened[dim(row, col)]) {
+            return;
+        }
+
+        opened[dim(row, col)] = true;
+        openedQnt += 1;
+
+        if (isValid(row - 1, col) && opened[dim(row - 1, col)]) {
+            uf.union(dim(row - 1, col), dim(row, col));
+        }
+        if (isValid(row + 1, col) && opened[dim(row + 1, col)]) {
+            uf.union(dim(row + 1, col), dim(row, col));
+        }
+        if (isValid(row, col-1) && opened[dim(row, col - 1)]) {
+            uf.union(dim(row, col - 1), dim(row, col));
+        }
+        if (isValid(row, col + 1) && opened[dim(row, col - 1)]) {
+            uf.union(dim(row, col + 1), dim(row, col));
+        }
+    }
+
+    private int dim(int row, int col) {
+        return (row - 1) * n + (col - 1);
+    }
+
+    private boolean isValid(int row, int col) {
+        return dim(row, col) >= 0 && dim(row, col) < n * n + 2;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        return opened[dim(row, col)];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return true;
+        return uf.connected(n * n, dim(row, col));
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        return openedQnt;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return false;
+        return isFull(n, n + 2);
     }
 
     // test client (optional)
---
+++ algo/algo-princeton/1-percolation/src/main/java/PercolationTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PercolationTest {
+
+    @Test
+    void test2by2BlockedGrid() {
+        Percolation p = new Percolation(2);
+        Assertions.assertFalse(p.percolates());
+        Assertions.assertFalse(p.isOpen(1, 1));
+        Assertions.assertFalse(p.isOpen(1, 2));
+        Assertions.assertFalse(p.isOpen(2, 1));
+        Assertions.assertFalse(p.isOpen(2, 2));
+        Assertions.assertEquals(p.numberOfOpenSites(), 0);
+        Assertions.assertFalse(p.isFull(1, 1));
+        Assertions.assertFalse(p.isFull(1, 2));
+        Assertions.assertFalse(p.isFull(2, 1));
+        Assertions.assertFalse(p.isFull(2, 2));
+    }
+}
