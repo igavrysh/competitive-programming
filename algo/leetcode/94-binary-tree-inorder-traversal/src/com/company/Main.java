5597a9710648c347da1f5bd4225d57577dd05e67
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-08 15:52:33 -0700

    94 binary tree inorder traversal

--- algo/leetcode/94-binary-tree-inorder-traversal/src/com/company/Main.java
+++ algo/leetcode/94-binary-tree-inorder-traversal/src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
+    public static void test1() {
+        TreeNode tree = new TreeNode(
+                1,
+                null,
+                new TreeNode(
+                        2,
+                        new TreeNode(3),
+                        null));
+        Solution solution = new Solution();
+        List<Integer> output = solution.inorderTraversal(tree);
+        Integer[] expectedResult = new Integer[]{1, 3, 2};
+        boolean checked = Arrays.equals(output.toArray(), expectedResult);
+        System.out.println(checked ? "test1 passed" : "test1 failed");
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        test1();
     }
 }
--- algo/leetcode/94-binary-tree-inorder-traversal/src/com/company/Solution.java
+++ algo/leetcode/94-binary-tree-inorder-traversal/src/com/company/Solution.java
@@ -1,4 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> lhs = new ArrayList<>();
+    if (root == null) {
+      return lhs;
+    }
+
+    if (root.left != null) {
+      lhs = inorderTraversal(root.left);
+    }
+
+    List<Integer> rhs = new ArrayList<>();
+    if (root.right != null) {
+      rhs = inorderTraversal(root.right);
+    }
+
+    lhs.add(root.val);
+    lhs.addAll(rhs);
+    return lhs;
+  }
 }
