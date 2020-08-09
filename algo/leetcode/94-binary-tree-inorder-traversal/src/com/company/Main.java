201dd2bc460b94912b5e3f2a4b3a06c33bb4b6bb
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-08 17:46:29 -0700

    Alternative solution to problem 94 binary tree inorder traversal

--- algo/leetcode/94-binary-tree-inorder-traversal/src/com/company/Main.java
+++ algo/leetcode/94-binary-tree-inorder-traversal/src/com/company/Main.java
@@ -22,7 +22,23 @@ public class Main {
         System.out.println(checked ? "test1 passed" : "test1 failed");
     }
 
+    public static void test2() {
+        TreeNode tree = new TreeNode(
+                1,
+                null,
+                new TreeNode(
+                        2,
+                        new TreeNode(3),
+                        null));
+        SolutionResultPassedAsArgument solution = new SolutionResultPassedAsArgument();
+        List<Integer> output = solution.inorderTraversal(tree);
+        Integer[] expectedResult = new Integer[]{1, 3, 2};
+        boolean checked = Arrays.equals(output.toArray(), expectedResult);
+        System.out.println(checked ? "test2 passed" : "test2 failed");
+    }
+
     public static void main(String[] args) {
         test1();
+        test2();
     }
 }
---
+++ algo/leetcode/94-binary-tree-inorder-traversal/src/com/company/SolutionResultPassedAsArgument.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionResultPassedAsArgument {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    inorderTraversal(root, result);
+    return result;
+  }
+
+  public void inorderTraversal(TreeNode root, List<Integer> result) {
+    if (root != null) {
+      if (root.left != null) {
+        inorderTraversal(root.left, result);
+      }
+      result.add(root.val);
+      if (root.right != null) {
+        inorderTraversal(root.right, result);
+      }
+    }
+  }
+}
