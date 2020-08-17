9e94e3ff61b853d564c2419e375aeb3c07cf366f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-17 08:05:06 -0700

    94-binary-tree-inorder-traversal morris traversal partial solution

--- algo/leetcode/99-recover-binary-search-tree/src/com/company/Solution.java
+++ algo/leetcode/99-recover-binary-search-tree/src/com/company/Solution.java
@@ -1,4 +1,42 @@
 package com.company;
 
-public class Solution {
+class Solution {
+  TreeNode m1 = null;
+  TreeNode m2 = null;
+  boolean foundM1 = false;
+  boolean foundM2 = false;
+  public void recoverTree(TreeNode root) {
+    inorderForward(root); // forward
+    inorderBackward(root); // backward
+
+    int temp = m1.val;
+    m1.val = m2.val;
+    m2.val = temp;
+  }
+
+  public void inorderForward(TreeNode root){
+    if(root == null) return;
+    inorderForward(root.left);
+
+    if(m1 != null && m1.val > root.val){
+      foundM1 = true;
+      return;
+    }
+    if(!foundM1)
+      m1 = root;
+    inorderForward(root.right);
+  }
+
+  public void inorderBackward(TreeNode root){
+    if(root == null) return;
+    inorderBackward(root.right);
+    if(m2 != null && m2.val < root.val){
+      foundM2 = true;
+      return;
+    }
+
+    if(!foundM2)
+      m2 = root;
+    inorderBackward(root.left);
+  }
 }
