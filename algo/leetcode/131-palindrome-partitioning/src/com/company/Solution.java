6cc63b6e253ef7ff27241889132cdc708529490f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-21 01:11:01 -0700

    Add template for 131-palindrome-partitioning

---
+++ algo/leetcode/131-palindrome-partitioning/src/com/company/Solution.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+  public List<List<String>> partition(String s) {
+    return new ArrayList<>();
+
+  }
+}
