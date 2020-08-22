bcc3d2289204a7ba7f69afb080b0314f79251d6d
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-21 18:47:36 -0700

    131-palindrome-partitioning my own solution

--- algo/leetcode/131-palindrome-partitioning/src/com/company/Main.java
+++ algo/leetcode/131-palindrome-partitioning/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
+  public static void test1() {
+    Solution solution = new Solution();
+    List<List<String>> output = solution.partition("aab");
+    boolean passed = output.contains(Arrays.asList(new String[]{"a", "a", "b"}))
+        && output.contains(Arrays.asList(new String[]{"aa", "b"}));
+    System.out.println("test1:" + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
   }
 }
--- algo/leetcode/131-palindrome-partitioning/src/com/company/Solution.java
+++ algo/leetcode/131-palindrome-partitioning/src/com/company/Solution.java
@@ -4,8 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-  public List<List<String>> partition(String s) {
-    return new ArrayList<>();
 
+  public List<List<String>> partition(String s) {
+    List<List<String>> output = new ArrayList<>();
+    partitionInternal(s, 0, s.length() - 1, output, new ArrayList<>());
+    return output;
+  }
+
+  private void partitionInternal(
+      String s,
+      int start,
+      int end,
+      List<List<String>> output,
+      List<String> currentOutput) {
+    while (start <= end) {
+      for (int i = 0; i <= (end - start); i++) {
+        if (isPalindrome(s, start, start + i)) {
+          currentOutput.add(s.substring(start, start + i + 1));
+          if (i == (end - start)) {
+            List<String> copy = new ArrayList<>();
+            copy.addAll(currentOutput);
+            output.add(copy);
+          } else {
+            partitionInternal(s, start + i + 1, end, output, currentOutput);
+          }
+          currentOutput.remove(currentOutput.size() - 1);
+        }
+      }
+      return;
+    }
+  }
+
+  private boolean isPalindrome(String s, int start, int end) {
+    for (int i = 0; i <= (int) ((end - start) / 2); i++) {
+      if (s.charAt(start + i) != s.charAt(end - i)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
