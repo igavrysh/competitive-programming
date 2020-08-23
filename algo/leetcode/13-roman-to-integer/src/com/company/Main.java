d7b406a2930ba7f4be42002097debc6697ad5765
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-23 11:59:59 -0700

    Solution for 13-roman-to-integer problem

--- algo/leetcode/13-roman-to-integer/src/com/company/Main.java
+++ algo/leetcode/13-roman-to-integer/src/com/company/Main.java
@@ -2,7 +2,41 @@ package com.company;
 
 public class Main {
 
+  private static void test1() {
+    String input = "III";
+    boolean passed = new Solution().romanToInt(input) == 3;
+    System.out.println("test1:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test2() {
+    String input = "IV";
+    boolean passed = new Solution().romanToInt(input) == 4;
+    System.out.println("test2:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test3() {
+    String input = "IX";
+    boolean passed = new Solution().romanToInt(input) == 9;
+    System.out.println("test3:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test4() {
+    String input = "LVIII";
+    boolean passed = new Solution().romanToInt(input) == 58;
+    System.out.println("test4:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test5() {
+    String input = "MCMXCIV";
+    boolean passed = new Solution().romanToInt(input) == 1994;
+    System.out.println("test5:" + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
   }
 }
--- algo/leetcode/13-roman-to-integer/src/com/company/Solution.java
+++ algo/leetcode/13-roman-to-integer/src/com/company/Solution.java
@@ -1,7 +1,35 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+
+  private Map<Character, Integer> dictionary;
+
+  Solution() {
+    dictionary = new HashMap<>();
+    dictionary.put('I', 1);
+    dictionary.put('V', 5);
+    dictionary.put('X', 10);
+    dictionary.put('L', 50);
+    dictionary.put('C', 100);
+    dictionary.put('D', 500);
+    dictionary.put('M', 1000);
+  }
+
   public int romanToInt(String s) {
-    return 0;
+    Character prevCharacter = null;
+    int result = 0;
+    for (char c : s.toCharArray()) {
+      if (prevCharacter != null) {
+        if (dictionary.get(prevCharacter) < dictionary.get(c)) {
+          result += -2 * dictionary.get(prevCharacter);
+        }
+      }
+      result += dictionary.get(c);
+      prevCharacter = c;
+    }
+    return result;
   }
 }
