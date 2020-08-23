67889bbec89ff96ba3578411251290df4f11b8b4
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-23 05:19:06 -0700

    Solution for 349-intersection-of-two-arrays

--- algo/leetcode/349-intersection-of-two-arrays/src/com/company/Main.java
+++ algo/leetcode/349-intersection-of-two-arrays/src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
+  public static void test1() {
+    Solution solution = new Solution();
+    int[] output = solution.intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2});
+    boolean passed = Arrays.equals(output, new int[]{2});
+    System.out.println("test1:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    Solution solution = new Solution();
+    Set<Integer> expected = new HashSet(Arrays.asList(new int[]{4, 9}));
+    Set<Integer> output = new HashSet(
+        Arrays.asList(
+            solution.intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})
+        )
+    );
+
+    boolean passed = output.contains(4)
+        && output.contains(9);
+    System.out.println("test2:" + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
   }
 }
--- algo/leetcode/349-intersection-of-two-arrays/src/com/company/Solution.java
+++ algo/leetcode/349-intersection-of-two-arrays/src/com/company/Solution.java
@@ -1,7 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Solution {
   public int[] intersection(int[] nums1, int[] nums2) {
-    return new int[]{1, 2};
+    Set<Integer> hNums1 = new HashSet<>();
+    for (int t : nums1) {
+      hNums1.add(t);
+    }
+    Set<Integer> output = new HashSet<>();
+    for (int t : nums2) {
+      if (hNums1.contains(t)) {
+        output.add(t);
+      }
+    }
+    int[] pureOutput = new int[output.size()];
+    int i = 0;
+    for (int t : output) {
+      pureOutput[i++] = t;
+    }
+    return pureOutput;
   }
 }
