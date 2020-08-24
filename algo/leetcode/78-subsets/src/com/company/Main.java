e5447b1ce0c6a17255c1c6aef009bfd60ec523c3
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-23 19:17:54 -0700

    Optimized solution for 78-subsets

--- algo/leetcode/78-subsets/src/com/company/Main.java
+++ algo/leetcode/78-subsets/src/com/company/Main.java
@@ -23,7 +23,24 @@ public class Main {
     System.out.println("test1:" +  (passed ? "passed" : "failed"));
   }
 
+  private static void test2() {
+    int[] nums = new int[]{1, 2, 3};
+    SolutionWithStartIndex solution = new SolutionWithStartIndex();
+    List<List<Integer>> output = solution.subsets(nums);
+    boolean passed = output.size() == 8
+        && output.contains(IntStream.of(new int[]{1}).boxed().collect(Collectors.toList()))
+        && output.contains(IntStream.of(new int[]{2}).boxed().collect(Collectors.toList()))
+        && output.contains(IntStream.of(new int[]{3}).boxed().collect(Collectors.toList()))
+        && output.contains(IntStream.of(new int[]{1, 2}).boxed().collect(Collectors.toList()))
+        && output.contains(IntStream.of(new int[]{1, 3}).boxed().collect(Collectors.toList()))
+        && output.contains(IntStream.of(new int[]{1, 2, 3}).boxed().collect(Collectors.toList()))
+        && output.contains(IntStream.of(new int[]{}).boxed().collect(Collectors.toList()));
+
+    System.out.println("test2:" +  (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
     test1();
+    test2();
   }
 }
--- algo/leetcode/78-subsets/src/com/company/Solution.java
+++ algo/leetcode/78-subsets/src/com/company/Solution.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
