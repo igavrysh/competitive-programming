187a4bda82019886dcf3718b16d1b98a59cc5aae
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-08-23 18:01:02 -0700

    Improve time for 129-sum-root-to-leaf-numbers problem

--- algo/leetcode/129-sum-root-to-leaf-numbers/src/com/company/Main.java
+++ algo/leetcode/129-sum-root-to-leaf-numbers/src/com/company/Main.java
@@ -25,9 +25,35 @@ public class Main {
     System.out.println("test3:" + (passed ? "passed" : "failed"));
   }
 
+  private static void test4() {
+    TreeNode input = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    SolutionSumInPlace solution = new SolutionSumInPlace();
+    int output = solution.sumNumbers(input);
+    boolean passed = output == 25;
+    System.out.println("test4:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test5() {
+    TreeNode input = new TreeNode(1);
+    SolutionSumInPlace solution = new SolutionSumInPlace();
+    int output = solution.sumNumbers(input);
+    boolean passed = output == 1;
+    System.out.println("test5:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test6() {
+    SolutionSumInPlace solution = new SolutionSumInPlace();
+    int output = solution.sumNumbers(null);
+    boolean passed = output == 0;
+    System.out.println("test6:" + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
     test1();
     test2();
     test3();
+    test4();
+    test5();
+    test6();
   }
 }
