2b76f28da2321525a41e55fec6c84814d74b69d6
ievgen_gavrysh <ievgen_gavrysh@epam.com>
2020-08-29 13:22:35 -0800

    Improved solution for 322-coin-change, still solution times out at leetcode

--- algo/leetcode/322-coin-change/src/com/company/Main.java
+++ algo/leetcode/322-coin-change/src/com/company/Main.java
@@ -10,7 +10,23 @@ public class Main {
     System.out.println("test1:" + (passed ? "passed" : "failed"));
   }
 
+  private static void test2() {
+    Solution solution = new Solution();
+    int output = solution.coinChange(new int[]{1}, 0);
+    boolean passed = output == 0;
+    System.out.println("test2:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test3() {
+    Solution solution = new Solution();
+    int output = solution.coinChange(new int[]{186, 419, 83, 408}, 6249);
+    System.out.println("test3 finished execution");
+  }
+
+
   public static void main(String[] args) {
-    test1();
+    //test1();
+    //test2();
+    test3();
   }
 }
--- algo/leetcode/322-coin-change/src/com/company/Solution.java
+++ algo/leetcode/322-coin-change/src/com/company/Solution.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Solution {
+
   public int coinChange(int[] coins, int amount) {
+    Arrays.sort(coins);
     int[] bestOptions = new int[amount + 1];
-    for (int i = 0; i <= amount ; i++) {
+    for (int i = 0; i <= amount; i++) {
       bestOptions[i] = -1;
     }
     bestOptions[0] = 0;
@@ -12,22 +16,23 @@ public class Solution {
   }
 
   private void coinChangeInternal(int[] coins, int amount, int[] bestOptions) {
-    int bestOptionsForAmount = 1000000;
+    int bestOptionsForAmount = -2;
     for (int i = 0; i < coins.length; i++) {
       int target = amount - coins[i];
       if (target < 0) {
-        continue;
+        break;
       }
       if (bestOptions[target] == -1) {
         coinChangeInternal(coins, target, bestOptions);
       }
       if (bestOptions[target] != -1) {
-        if (bestOptionsForAmount > (1 + bestOptions[target])) {
+        if (bestOptionsForAmount > (1 + bestOptions[target])
+            || bestOptionsForAmount == -2) {
           bestOptionsForAmount = 1 + bestOptions[target];
         }
       }
     }
-    if (bestOptionsForAmount != -1) {
+    if (bestOptionsForAmount != -1 && bestOptionsForAmount != -2) {
       bestOptions[amount] = bestOptionsForAmount;
     }
   }
