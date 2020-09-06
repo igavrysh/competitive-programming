31e7655db8d4afaadc16c3e75c03f523eff7d1e0
ievgen_gavrysh <ievgen.gavrysh@gmail.com>
2020-09-06 10:34:18 -0700

    Add working solution for 416-partition-equal-subset-sum

--- algo/leetcode/416-partition-equal-subset-sum/src/com/company/Main.java
+++ algo/leetcode/416-partition-equal-subset-sum/src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
   public static void test2() {
     Solution solution = new Solution();
     boolean passed = solution.canPartition(new int[] {1, 2, 3, 5}) == false;
-    System.out.println("test1:" + (passed ? "passed" : "failed"));
+    System.out.println("test2:" + (passed ? "passed" : "failed"));
   }
 
   public static void test3() {
@@ -23,12 +23,29 @@ public class Main {
         18,66,62,55,19,2,67,30,25,64,84,25,76,98,59,74,87,5,93,97,68,20,
         58,55,73,74,97,49,71,42,26,8,87,99,1,16,79
     }) == true;
-    System.out.println("test1:" + (passed ? "passed" : "failed"));
+    System.out.println("test3:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test4() {
+    Solution solution = new Solution();
+    int[] input = new int [] {
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,100
+    };
+    int totalSum = 0;
+    for (int i : input) {
+      totalSum += i;
+    }
+
+    boolean passed = solution.canPartition(input) == false;
+    System.out.println("test4:" + (passed ? "passed" : "failed"));
   }
 
   public static void main(String[] args) {
     test1();
     test2();
     test3();
+    test4();
   }
 }
--- algo/leetcode/416-partition-equal-subset-sum/src/com/company/Solution.java
+++ algo/leetcode/416-partition-equal-subset-sum/src/com/company/Solution.java
@@ -2,8 +2,6 @@ package com.company;
 
 public class Solution {
 
-  public static int EMPTY = -1;
-
   public boolean canPartition(int[] nums) {
     int totalSum = 0;
     for (int i = 0; i < nums.length; i++) {
@@ -14,41 +12,36 @@ public class Solution {
       return false;
     }
 
-    int sumToBuild = totalSum / 2;
-    return buildSum(nums, sumToBuild);
+    Boolean[][] freq = new Boolean[totalSum + 1][nums.length + 1];
+
+    return canPartition(nums, 0, totalSum, 0, freq);
   }
 
-  private boolean buildSum(int nums[], int sumToBuild) {
-    if (sumToBuild == 0) {
+  private boolean canPartition(int nums[], int index, int target, int currentSum, Boolean[][] freq) {
+    if (2 * currentSum == target) {
       return true;
     }
 
-    if (sumToBuild < 0) {
+    if (index >= nums.length || currentSum * 2 > target) {
       return false;
     }
-    boolean result = false;
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != EMPTY) {
-        int num_i = nums[i];
-        nums[i] = EMPTY;
 
-        boolean output = buildSum(nums, sumToBuild - num_i);
-
-        if (!output) {
-          output = buildSum(nums, sumToBuild);
-        } else {
-          nums[i] = num_i;
-          return true;
-        }
-
-        if (output) {
-          result = output;
-        }
-
-        nums[i] = num_i;
-      }
+    if (freq[currentSum][index] != null) {
+      return freq[currentSum][index];
     }
 
-    return result;
+    boolean lhs = canPartition(nums, index + 1, target, currentSum + nums[index], freq);
+    freq[currentSum + nums[index]][index + 1] = lhs;
+    if (lhs == true) {
+      return lhs;
+    }
+
+    boolean rhs = canPartition(nums, index + 1, target, currentSum, freq);
+    freq[currentSum][index + 1] = rhs;
+    if (rhs == true) {
+      return rhs;
+    }
+
+    return false;
   }
 }
