b59bd04ffb9eaae729b5ec749c52a49c28fd7d3f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-09-10 18:11:37 -0700

    Optimal iterative solution for 206-reverse-linked-list

--- algo/leetcode/206-reverse-linked-list/src/com/company/SolutionIterative.java
+++ algo/leetcode/206-reverse-linked-list/src/com/company/SolutionIterative.java
@@ -2,33 +2,15 @@ package com.company;
 
 public class SolutionIterative {
   public ListNode reverseList(ListNode head) {
-    if (head == null) {
-      return null;
-    }
-
-    ListNode newHead = null;
-    ListNode current = head.next;
-    ListNode previous = head;
-
-    if (current == null) {
-      return previous;
-    }
-
+    ListNode current = head;
+    ListNode previous = null;
     while (current != null) {
-      if (current.next == null) {
-        newHead = current;
-      }
-      if (previous == head) {
-        previous.next = null;
-      }
-
       ListNode currentNextTmp = current.next;
-
       current.next = previous;
       previous = current;
       current = currentNextTmp;
     }
-    return newHead;
+    return previous;
   }
 }
 
