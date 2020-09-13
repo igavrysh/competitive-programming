64f1997ea94183999fd5bf502cae5f28a2176655
ievgen_gavrysh <ievgen_gavrysh@epam.com>
2020-09-12 21:31:18 -0700

    Add priority queue

--- algo/leetcode/621-task-scheduler/src/com/company/Main.java
+++ algo/leetcode/621-task-scheduler/src/com/company/Main.java
@@ -2,7 +2,15 @@ package com.company;
 
 public class Main {
 
+  public static void test1() {
+    char[] tasks = new char[] { 'A', 'A', 'A', 'B', 'B', 'B' };
+    int n = 2;
+    Solution s = new Solution();
+    boolean passed = s.leastInterval(tasks, n) == 8;
+    System.out.println("test1:" + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
   }
 }
--- algo/leetcode/621-task-scheduler/src/com/company/Solution.java
+++ algo/leetcode/621-task-scheduler/src/com/company/Solution.java
@@ -1,7 +1,32 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
 public class Solution {
   public int leastInterval(char[] tasks, int n) {
+    int[] freq = new int[26];
+    for (int i = 0; i < freq.length; i++) {
+      freq[i] = 0;
+    }
+
+    Set<Character> uT = new HashSet<>();
+
+    for (char c : tasks) {
+      uT.add(c);
+      freq[c - 'A'] += 1;
+    }
+
+    Queue<Character> pQ = new PriorityQueue<>(
+        uT.size(),
+        (c1, c2) -> freq[c1 - 'A'] - freq[c2 - 'A']);
+    pQ.addAll(uT);
+
+
+
+
     return -1;
   }
 
