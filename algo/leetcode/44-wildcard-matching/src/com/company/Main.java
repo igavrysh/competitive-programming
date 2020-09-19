6c2ff67e212496a843bb9ca318606f1995efb482
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-09-19 16:20:05 -0700

    Brute force solution for 44-wildcar-matching

--- algo/leetcode/44-wildcard-matching/src/com/company/Main.java
+++ algo/leetcode/44-wildcard-matching/src/com/company/Main.java
@@ -2,7 +2,88 @@ package com.company;
 
 public class Main {
 
+  public static void test1() {
+    String s = "aa";
+    String p = "a";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == false;
+    System.out.println("test1:" + (passed ? "true" : "false"));
+  }
+
+  public static void test2() {
+    String s = "aa";
+    String p = "*";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == true;
+    System.out.println("test2:" + (passed ? "true" : "false"));
+  }
+
+  public static void test3() {
+    String s = "cb";
+    String p = "?a";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == false;
+    System.out.println("test3:" + (passed ? "true" : "false"));
+  }
+
+  public static void test4() {
+    String s = "adceb";
+    String p = "*a*b";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == true;
+    System.out.println("test4:" + (passed ? "true" : "false"));
+  }
+
+  public static void test5() {
+    String s = "acdcb";
+    String p = "a*c?b";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == false;
+    System.out.println("test5:" + (passed ? "true" : "false"));
+  }
+
+  public static void test6() {
+    String s = "";
+    String p = "*";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == true;
+    System.out.println("test6:" + (passed ? "true" : "false"));
+  }
+
+  public static void test7() {
+    String s = "abbabbbaabaaabbbbbabbabbabbbabbaaabbbababbabaaabbab";
+    String p = "*aabb***aa**a******aa*";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == true;
+    System.out.println("test7:" + (passed ? "true" : "true"));
+  }
+
+  public static void test8() {
+    String s = "abbabbbaabaaabbbbbabbabbabbbabbaaabbbababbabaaabbab";
+    String p = "*aabb*aa*a*aa*";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == true;
+    System.out.println("test8:" + (passed ? "true" : "false"));
+  }
+
+  public static void test9() {
+    String s = "abbbba";
+    String p = "a******a";
+    Solution solution = new Solution();
+    boolean passed = solution.isMatch(s, p) == true;
+    System.out.println("test9:" + (passed ? "true" : "false"));
+  }
+
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    //test7();
+    test8();
+    test9();
   }
 }
--- algo/leetcode/44-wildcard-matching/src/com/company/Solution.java
+++ algo/leetcode/44-wildcard-matching/src/com/company/Solution.java
@@ -1,7 +1,49 @@
 package com.company;
 
 class Solution {
+  enum DPValues {
+    NOT_SET, MATCHED, MISMATCHED;
+  }
+
   public boolean isMatch(String s, String p) {
+    return isM(0, 0, s, p);
+  }
+
+  private boolean isM(int iS, int iP, String s, String p) {
+    if (iS >= s.length() && iP >= p.length()) {
+      return true;
+    }
+
+    if (iP < p.length() && p.charAt(iP) == '*') {
+      boolean l1 = false;
+      if (iS < s.length() && iP < p.length()) {
+        l1 = isM(iS+1, iP+1, s, p);
+      }
+      boolean l2 = false;
+      if (iS < s.length()) {
+        l2 = isM(iS+ 1, iP, s, p);
+      }
+      boolean l3 = false;
+      if (iP < p.length()) {
+        l3 = isM(iS, iP+1, s, p);
+      }
+
+      return l1 || l2 || l3;
+    }
+
+    if ((iS == s.length() && iP != p.length())
+      || (iS != s.length() && iP == p.length())
+    ) {
+      return false;
+    }
+
+    if (p.charAt(iP) == '?') {
+      return isM(iS+1, iP+1, s, p);
+    }
+
+    if (p.charAt(iP) == s.charAt(iS)) {
+      return isM(iS+1, iP+1, s, p);
+    }
     return false;
   }
 }
