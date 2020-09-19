380959810dd7de645345b6b03a0a4a89dadc843e
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-09-19 11:25:16 -0700

    Add optimized brute force solution for 301-remove-invalid-parentheses

--- algo/leetcode/301-remove-invalid-parentheses/src/com/company/Solution.java
+++ algo/leetcode/301-remove-invalid-parentheses/src/com/company/Solution.java
@@ -2,10 +2,26 @@ package com.company;
 
 import java.util.*;
 
-class Solution {
+public class Solution {
   public List<String> removeInvalidParentheses(String s) {
+    int l = 0;
+    int r = 0;
+    for (char c : s.toCharArray()) {
+      if (c == '(') {
+        l++;
+      }
+
+      if (c == ')') {
+        if (l == 0) {
+          r++;
+        } else {
+          l--;
+        }
+      }
+    }
+
     Map<Integer, Set<String>> output = new HashMap<>();
-    generateValidParentheses(s, 0, 0, 0, 0, "", output);
+    generateValidParentheses(s, 0, 0, 0, l, r, 0, "", output);
 
     if (output.size() == 0) {
       return Arrays.asList(new String[] {""});
@@ -24,11 +40,12 @@ class Solution {
     return new ArrayList<>(output.get(gMin));
   }
 
+
   private void generateValidParentheses(
-      String s, int i, int l, int r, int removedBrackets, String acc, Map<Integer, Set<String>> output
+      String s, int i, int l, int r, int remL, int remR, int removedBrackets, String acc, Map<Integer, Set<String>> output
   ) {
     if (i >= s.length()) {
-      if (l == r) {
+      if (l == r && remL == 0 && remR == 0) {
         Set<String> o = output.get(removedBrackets);
         if (o == null) {
           o = new HashSet<>();
@@ -40,18 +57,23 @@ class Solution {
     }
 
     if (s.charAt(i) != '('
-      && s.charAt(i) != ')'
+        && s.charAt(i) != ')'
     ) {
-      generateValidParentheses(s, i+1, l, r, removedBrackets, acc + s.charAt(i), output);
+      generateValidParentheses(s, i+1, l, r, remL, remR, removedBrackets, acc + s.charAt(i), output);
     } else {
       if (s.charAt(i) == '(') {
-        generateValidParentheses(s, i+1, l+1, r, removedBrackets, acc + '(', output);
-        generateValidParentheses(s, i+1, l, r, removedBrackets+1, acc, output);
+        generateValidParentheses(s, i+1, l+1, r, remL, remR, removedBrackets, acc + '(', output);
+        if (remL > 0) {
+          generateValidParentheses(s, i + 1, l, r, remL-1, remR,removedBrackets + 1, acc, output);
+        }
       } else if (s.charAt(i) == ')') {
         if (l > r) {
-          generateValidParentheses(s, i+1, l, r+1, removedBrackets, acc + ')', output);
+          generateValidParentheses(s, i+1, l, r+1, remL, remR, removedBrackets, acc + ')', output);
         }
-        generateValidParentheses(s, i+1, l, r, removedBrackets+1, acc, output);
+
+        if (remR > 0) {
+          generateValidParentheses(s, i+1, l, r, remL, remR - 1, removedBrackets+1, acc, output);
+        }
       }
     }
   }
