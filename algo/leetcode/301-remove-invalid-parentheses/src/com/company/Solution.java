278debc146c379c0ea6c9a682a6823bfdbb796ab
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-09-19 10:15:26 -0700

    Brute force solution for 301-remove-invalid-parentheses

--- algo/leetcode/301-remove-invalid-parentheses/src/com/company/Solution.java
+++ algo/leetcode/301-remove-invalid-parentheses/src/com/company/Solution.java
@@ -1,9 +1,58 @@
 package com.company;
 
-import java.util.List;
+import java.util.*;
 
 class Solution {
   public List<String> removeInvalidParentheses(String s) {
-    return null;
+    Map<Integer, Set<String>> output = new HashMap<>();
+    generateValidParentheses(s, 0, 0, 0, 0, "", output);
+
+    if (output.size() == 0) {
+      return Arrays.asList(new String[] {""});
+    }
+
+    int gMin = -1;
+    for (Integer i : output.keySet()) {
+      if (gMin == -1) {
+        gMin = i;
+      }
+
+      if (gMin > i) {
+        gMin = i;
+      }
+    }
+    return new ArrayList<>(output.get(gMin));
+  }
+
+  private void generateValidParentheses(
+      String s, int i, int l, int r, int removedBrackets, String acc, Map<Integer, Set<String>> output
+  ) {
+    if (i >= s.length()) {
+      if (l == r) {
+        Set<String> o = output.get(removedBrackets);
+        if (o == null) {
+          o = new HashSet<>();
+          output.put(removedBrackets, o);
+        }
+        o.add(acc);
+      }
+      return;
+    }
+
+    if (s.charAt(i) != '('
+      && s.charAt(i) != ')'
+    ) {
+      generateValidParentheses(s, i+1, l, r, removedBrackets, acc + s.charAt(i), output);
+    } else {
+      if (s.charAt(i) == '(') {
+        generateValidParentheses(s, i+1, l+1, r, removedBrackets, acc + '(', output);
+        generateValidParentheses(s, i+1, l, r, removedBrackets+1, acc, output);
+      } else if (s.charAt(i) == ')') {
+        if (l > r) {
+          generateValidParentheses(s, i+1, l, r+1, removedBrackets, acc + ')', output);
+        }
+        generateValidParentheses(s, i+1, l, r, removedBrackets+1, acc, output);
+      }
+    }
   }
 }
