377fec55ee6b6fc40bee3576026f472bbf2de1a1
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-09-22 09:46:00 -0700

    Add coing-change & longest-increasing-subsequence problems to medium top interview questions

---
+++ algo/leetcode/dynamic-programming/medium/coin-change/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+  private static void test1() {
+    Solution solution = new Solution();
+    int output = solution.coinChange(new int[]{1, 2, 5}, 11);
+    System.out.println("output:" + Integer.toString(output));
+    boolean passed = output == 3;
+    System.out.println("test1:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test2() {
+    Solution solution = new Solution();
+    int output = solution.coinChange(new int[]{1}, 0);
+    boolean passed = output == 0;
+    System.out.println("test2:" + (passed ? "passed" : "failed"));
+  }
+
+  private static void test3() {
+    Solution solution = new Solution();
+    int output = solution.coinChange(new int[]{186, 419, 83, 408}, 6249);
+    boolean passed = output == 20;
+    System.out.println("test3:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void main(String[] args) {
+    test1();
+    test2();
+    test3();
+  }
+}
