33c4f281eeab25f8df8851f0d428659b175f016b
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-10-01 10:33:32 -0700

    Add iterative solution for 121-best-time-to-buy-and-sell-stock

--- algo/leetcode/121-best-time-to-buy-and-sell-stock/src/com/company/Main.java
+++ algo/leetcode/121-best-time-to-buy-and-sell-stock/src/com/company/Main.java
@@ -15,8 +15,37 @@ public class Main {
     System.out.println("test2:" + (passed ? "passed" : "failed"));
   }
 
+  public static void test3() {
+    SolutionIterative solution = new SolutionIterative();
+    int output = solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+    boolean passed = output == 5;
+    System.out.println("test3:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test4() {
+    SolutionIterative solution = new SolutionIterative();
+    boolean passed = solution.maxProfit(new int[]{7, 6, 4, 3, 1}) == 0;
+    System.out.println("test4:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test5() {
+    SolutionIterative solution = new SolutionIterative();
+    boolean passed = solution.maxProfit(new int[]{1}) == 0;
+    System.out.println("test5:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test6() {
+    SolutionIterative solution = new SolutionIterative();
+    boolean passed = solution.maxProfit(new int[]{1, 2}) == 1;
+    System.out.println("test6:" + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
     test1();
     test2();
+    test3();
+    test4();
+    test5();
+    test6();
   }
 }
---
+++ algo/leetcode/121-best-time-to-buy-and-sell-stock/src/com/company/SolutionIterative.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class SolutionIterative {
+
+  public int maxProfit(int[] prices) {
+    if (prices.length <= 1) {
+      return 0;
+    }
+
+    int[] increments = new int[prices.length-1];
+    for (int i = 0; i < increments.length; i++) {
+      increments[i] = prices[i+1] - prices[i];
+    }
+
+    int[] dp = new int[increments.length];
+    dp[0] = increments[0];
+    for (int i = 1; i < increments.length; i++) {
+      dp[i] = Math.max(dp[i - 1] + increments[i], increments[i]);
+    }
+
+    int output = 0;
+    for (int i = 0; i < dp.length; i++) {
+      if (output < dp[i]) {
+        output = dp[i];
+      }
+    }
+
+    return output;
+  }
+}
