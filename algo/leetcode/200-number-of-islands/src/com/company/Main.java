59c70e74098ed891a4a825b6ca7a07094c4c2853
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-10-09 22:14:46 -0700

    DFS solution for 200-number-of-islands

--- algo/leetcode/200-number-of-islands/src/com/company/Main.java
+++ algo/leetcode/200-number-of-islands/src/com/company/Main.java
@@ -52,10 +52,64 @@ public class Main {
     System.out.println("test4:" + (passed ? "passed" : "failed"));
   }
 
+  public static void test5() {
+    char[][] input = {
+        {'1','1','1','1','0'},
+        {'1','1','0','1','0'},
+        {'1','1','0','0','0'},
+        {'0','0','0','0','0'}
+    };
+    SolutionDFS s = new SolutionDFS();
+    int output = s.numIslands(input);
+    boolean passed = output == 1;
+    System.out.println("test5:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test6() {
+    char[][] input = {
+        {'1','1','0','0','0'},
+        {'0','0','1','0','0'},
+        {'0','0','1','0','0'},
+        {'0','0','0','1','1'}
+    };
+    SolutionDFS s = new SolutionDFS();
+    int output = s.numIslands(input);
+    boolean passed = output == 3;
+    System.out.println("test6:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test7() {
+    char[][] input = {
+        {'1','1','0','0','0'},
+        {'1','1','0','0','0'},
+        {'0','0','1','0','0'},
+        {'0','0','0','1','1'}
+    };
+    SolutionDFS s = new SolutionDFS();
+    int output = s.numIslands(input);
+    boolean passed = output == 3;
+    System.out.println("test7:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test8() {
+    char[][] input = {
+        {'1'},
+        {'1'}
+    };
+    SolutionDFS s = new SolutionDFS();
+    int output = s.numIslands(input);
+    boolean passed = output == 1;;
+    System.out.println("test8:" + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    //test1();
-    //test2();
-    //test3();
+    test1();
+    test2();
+    test3();
     test4();
+    test5();
+    test6();
+    test7();
+    test8();
   }
 }
