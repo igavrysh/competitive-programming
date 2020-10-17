50fa05e15339e9b0a911a5582b6d1b47da9140d9
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-10-17 09:56:55 -0700

    Optimized solution for 15-3sum problem

--- algo/leetcode/15-3sum/src/com/company/SolutionOptimized.java
+++ algo/leetcode/15-3sum/src/com/company/SolutionOptimized.java
@@ -9,27 +9,43 @@ public class SolutionOptimized {
     if (nums.length == 0) {
       return new ArrayList<>();
     }
-
     List<List<Integer>> output = new ArrayList<>();
     Arrays.sort(nums);
-    for (int i = 0; i < nums.length-2; i++) {
-      int l = i+1;
-      int r = nums.length-1;
-      while (l<r) {
+    int i = 2;
+    while (i < nums.length) {
+      if (i+1 < nums.length && nums[i] == nums[i+1]) {
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+          i+=1;
+        }
+      }
+      int l = 0;
+      int r = i-1;
+
+      while (l < r) {
         if (nums[i] + nums[l] + nums[r] == 0) {
           output.add(Arrays.asList(new Integer[]{nums[l], nums[r], nums[i]}));
         }
-        if (l+1 < nums.length && nums[l] == nums[l+1]) {
+
+        if ((nums[l] + nums[r]) >= -nums[i]) {
+          if (nums[r] == nums[r-1]) {
+            while (r - 1 >= 0 && nums[r] == nums[r-1]) {
+              r-= 1;
+            }
+          }
+          r--;
+
+        } else if ((nums[l] + nums[r]) < -nums[i]) {
+          if (nums[l] == nums[l+1]) {
+            while (l+1 <= i-1 && nums[l] == nums[l+1]) {
+              l+= 1;
+            }
+          }
           l++;
         }
-        if (r-1 >= 0 && nums[r] == nums[r-1]) {
-          r--;
-        }
-        l++;
-        r--;
       }
-    }
 
+      i++;
+    }
     return output;
   }
 }
