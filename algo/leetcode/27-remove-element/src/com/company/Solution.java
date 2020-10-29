b1e02a15d802ad0dbef5d00c081581adf0f66f4a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-10-28 17:00:36 -0700

    Fix small typo in variable name

--- algo/leetcode/27-remove-element/src/com/company/Solution.java
+++ algo/leetcode/27-remove-element/src/com/company/Solution.java
@@ -3,15 +3,15 @@ package com.company;
 public class Solution {
   public int removeElement(int[] nums, int val) {
     int internalIndex = 0;
-    int lenth = 0;
+    int length = 0;
     for (int i = 0; i < nums.length; i++) {
       if (nums[i] != val) {
         nums[internalIndex] = nums[i];
         internalIndex++;
-        lenth++;
+        length++;
       }
     }
-    return lenth;
+    return length;
   }
 }
 
