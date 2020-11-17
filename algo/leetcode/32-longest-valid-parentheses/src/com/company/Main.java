a6ba512756a08a8e65f81a7403ed6a97faa1e220
new <new@pop-os.localdomain>
2020-11-17 09:05:39 -0800

    solution for 33-search-in-rotated-sorted-array

---
+++ algo/leetcode/32-longest-valid-parentheses/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+
+  public static void test1() {
+    Solution s = new Solution();
+    int output = s.longestValidParentheses("(()");
+    boolean passed = output == 2;
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    Solution s = new Solution();
+    int output = s.longestValidParentheses(")()())");
+    boolean passed = output == 4;
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test3() {
+    Solution s = new Solution();
+    int output = s.longestValidParentheses("");
+    boolean passed = output == 0;
+    System.out.println("test3: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test4() {
+    Solution s = new Solution();
+    int output = s.longestValidParentheses("()(())");
+    boolean passed = output == 6;
+    System.out.println("test4: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test5() {
+    Solution s = new Solution();
+    int output = s.longestValidParentheses(")()())()()(");
+    boolean passed = output == 6;
+    System.out.println("test5: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void main(String[] args) {
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+  }
+}
--- algo/leetcode/32-longest-valid-parentheses/src/com/company/Solution.java
+++ algo/leetcode/32-longest-valid-parentheses/src/com/company/Solution.java
@@ -1,2 +1,30 @@
-package com.company;public class Solution {
+package com.company;
+
+public class Solution {
+  public int longestValidParentheses(String s) {
+    if (s.length() == 0) {
+      return 0;
+    }
+    int[] DP = new int[s.length()];
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) == ')' && s.charAt(i-1) == '(') {
+        DP[i] = 2 + (i-2 >= 0 ? DP[i-2] : 0);
+      }
+      if (s.charAt(i) == ')' && s.charAt(i-1) == ')') {
+        int remainder = (i-1)-DP[i-1]-1 >= 0 ? DP[(i-1)-DP[i-1]-1] : 0;
+        DP[i] = DP[i-1]
+            + (i-1-DP[i-1] < 0
+              ? 0
+              : s.charAt((i-1)-DP[i-1]) == '(' ? 2 + remainder : 0);
+      }
+    }
+
+    int ans = DP[0];
+    for (int i = 1; i < DP.length; i++) {
+      if (DP[i] > ans) {
+        ans = DP[i];
+      }
+    }
+    return ans;
+  }
 }
