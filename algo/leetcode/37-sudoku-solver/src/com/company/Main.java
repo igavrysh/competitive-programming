d64c3500e969ca6bfeab3ed63e3911d3c0be08d5
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-02-18 06:56:49 -0800

    add test for problem 37-sudoku-solver

--- algo/leetcode/37-sudoku-solver/src/com/company/Main.java
+++ algo/leetcode/37-sudoku-solver/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
   public static void test1() {
@@ -15,9 +17,27 @@ public class Main {
             {'.','.','.','4','1','9','.','.','5'},
             {'.','.','.','.','8','.','.','7','9'}
         };
+    char[][] expectedResultBoard = 
+        {
+            {'5','3','4','6','7','8','9','1','2'},
+            {'6','7','2','1','9','5','3','4','8'},
+            {'1','9','8','3','4','2','5','6','7'},
+            {'8','5','9','7','6','1','4','2','3'},
+            {'4','2','6','8','5','3','7','9','1'},
+            {'7','1','3','9','2','4','8','5','6'},
+            {'9','6','1','5','3','7','2','8','4'},
+            {'2','8','7','4','1','9','6','3','5'},
+            {'3','4','5','2','8','6','1','7','9'}
+        };
     Solution s = new Solution();
     s.solveSudoku(board);
-    int t = 0;
+    boolean passed = true;
+    for (int i=0; i<board.length; i++) {
+      if (!Arrays.equals(board[i], expectedResultBoard[i])) {
+        passed = false;
+      }
+    }
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
   }
 
   public static void main(String[] args) {
