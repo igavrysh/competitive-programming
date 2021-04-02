e72e27f58f4dda383aeb7f5bfd67031e3ee9cae0
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-02 15:32:11 -0700

    leetcode.solutions solution for problem 3-longest-unique-substring

---
+++ algo/leetcode/3-longest-unique-substring/src/leetcode/solutions/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.solutions;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+  public int lengthOfLongestSubstring(String s) {
+    int maxLengthReached = 0;
+    Set<Character> elements = new HashSet<>();
+    int start = 0;
+    int end = 0;
+    while (end < s.length() && start <= end) {
+      int delta = end - start;
+      if (delta > maxLengthReached) {
+        maxLengthReached = delta;
+      }
+      Character element = s.charAt(end);
+      if (elements.contains(element)) {
+        Character elementToRemove = s.charAt(start);
+        elements.remove(elementToRemove);
+        start += 1;
+      } else {
+        elements.add(element);
+        end += 1;
+      }
+    }
+    return maxLengthReached;
+  }
+}
+//--------------------------------------------------
