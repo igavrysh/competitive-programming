dd83d88340ef4caf2dfadb9a8cbd83ce6c13eb75
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-02 10:05:09 -0700

    leetcode.solutions for problem 2-add-two-numbers

---
+++ algo/leetcode/2-add-two-numbers/src/leetcode/solutions/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.solutions;
+
+public class Solution {
+  public ListNode addTwoNumbers(ListNode l1,
+                                ListNode l2)
+  {
+    int carry = 0;
+    ListNode resN = new ListNode(0);
+    ListNode currN = resN;
+    while (l1 != null || l2 != null || carry != 0) {
+      int acc = (l1 == null ? 0 : l1.val)
+          + (l2 == null ? 0 : l2.val)
+          + carry;
+      ListNode node = new ListNode(acc % 10);
+      carry = acc / 10;
+      currN.next = node;
+      currN = currN.next;
+      l1 = l1 != null ? l1.next : null;
+      l2 = l2 != null ? l2.next : null;
+    }
+    return resN.next;
+  }
+}
+//--------------------------------------------------
