4099451d7b80cb4356b8978455136cc7fb51008a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-12 20:21:53 -0700

    solution for problem 143 reorder list

---
+++ algo/leetcode/143-reorder-list/src/com/company/Solution.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Solution {
+  public void reorderList(ListNode head) {
+    // to know the full length of the list
+    int length = 0;
+    ListNode n = head;
+    while (n != null) {
+      n = n.next;
+      length++;
+    }
+
+    // to cut in half
+    int m = length / 2;
+
+    // add second half to a stack
+    n = head;
+    Stack<ListNode> stack = new Stack<>();
+    int i = 0;
+    while (n != null) {
+      if (i >= m) {
+        stack.push(n);
+      }
+      n = n.next;
+      i++;
+    }
+
+    // go from the begining to the middle point, inserting elements from stack on even  places
+    n = head;
+    i = 0;
+    while (n != null) {
+      ListNode stackHead = stack.pop();
+      ListNode tmp = stack.size() == 0 ? null : n.next;
+      n.next = stackHead;
+      stackHead.next = tmp;
+      n = tmp;
+      i++;
+    }
+  }
+}
