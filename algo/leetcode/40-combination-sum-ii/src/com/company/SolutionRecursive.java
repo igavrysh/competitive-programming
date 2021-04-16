dba36b0ee63f37762eba0dff2fb471ebb80d0e42
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-16 14:30:10 -0700

    improved solution for combination sum 2 problem

--- algo/leetcode/40-combination-sum-ii/src/com/company/SolutionRecursive.java
+++ algo/leetcode/40-combination-sum-ii/src/com/company/SolutionRecursive.java
@@ -2,19 +2,19 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+
 import java.util.List;
 
 public class SolutionRecursive {
   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
     Arrays.sort(candidates);
     List<List<Integer>> output = new ArrayList<>();
-    comb(candidates, target, 0, 0, new ArrayList<>(), "", output, new HashSet<>(), target);
+    comb(candidates, target, 0, 0, new ArrayList<>(), output, target);
     return output;
   }
 
-  private void comb(int[] candidates, int trg, int startIndex, int accSum, List<Integer> acc, String accStr,
-                    List<List<Integer>> output, HashSet<String> outputHash, int initTrg) {
+  private void comb(int[] candidates, int trg, int startIndex, int accSum, List<Integer> acc,
+                    List<List<Integer>> output, int initTrg) {
     if (trg == 0) {
       output.add(new ArrayList<>(acc));
       return;
@@ -25,15 +25,12 @@ public class SolutionRecursive {
     }
 
     for (int i = startIndex; i < candidates.length; i++) {
-      String newHash = accStr + "_" + candidates[i];
-
-      if (!outputHash.contains(newHash)) {
-        acc.add(candidates[i]);
-        outputHash.add(newHash);
-        comb(candidates, trg - candidates[i], i+1, accSum + candidates[i], acc, newHash, output, outputHash,
-            initTrg);
-        acc.remove(acc.size() - 1);
+      if (i != startIndex && i-1>=0 && candidates[i] == candidates[i-1]) {
+        continue;
       }
+      acc.add(candidates[i]);
+      comb(candidates, trg - candidates[i], i+1, accSum + candidates[i], acc, output, initTrg);
+      acc.remove(acc.size() - 1);
     }
   }
 }
