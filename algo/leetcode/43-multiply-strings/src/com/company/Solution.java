605aef5c29bb8d2a371b8d7e248d7a518042bca7
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-18 21:46:56 -0700

    optimized solution for problem 43-multiple-strings

--- algo/leetcode/43-multiply-strings/src/com/company/Solution.java
+++ algo/leetcode/43-multiply-strings/src/com/company/Solution.java
@@ -4,33 +4,31 @@ public class Solution {
   public String multiply(String num1, String num2) {
     int len1 = num1.length();
     int len2 = num2.length();
-    Integer []result = new Integer[len1 + len2];
-    for (int i = 0; i < result.length; i++) {
-      result[i] = null;
+    int[] n1 = new int[num1.length()];
+    for (int i = 0; i < n1.length; i++) {
+      n1[i] = num1.charAt(i) - '0';
     }
+    int []result = new int[len1 + len2];
     for (int i = 0; i < len2; i++) {
       for (int j = 0; j < len1; j++) {
-        int toAppend = (num1.charAt(len1-1-j) - '0') * (num2.charAt(len2-1-i) - '0');
-        result[i+j] = (result[i+j] == null ? 0 : result[i+j])
-            + toAppend;
+        result[i+j] += (n1[len1-1-j]) * (num2.charAt(len2-1-i) - '0');
       }
     }
     int carry = 0;
+    for (int i = 0; i < result.length; i++) {
+      int tmp = result[i] + carry;
+      result[i] = tmp % 10;
+      carry = tmp / 10;
+    }
+    boolean startAppending = false;
     StringBuilder sb = new StringBuilder();
     for (int i = result.length-1; i >= 0; i--) {
-      if (result[i] == null || result[i] == 0 ) {
-        result[i] = null;
-      } else {
-        break;
+      if (result[i] != 0) {
+        startAppending = true;
       }
-    }
-    for (int i = 0; i < result.length; i++) {
-      if (result[i] == null && carry == 0) {
-        break;
+      if (startAppending) {
+        sb.append(result[i]);
       }
-      int currRes = result[i] == null ? 0 : result[i];
-      sb.insert(0, (currRes + carry) % 10);
-      carry = (currRes + carry - ((currRes+ carry) % 10)) / 10;
     }
     return sb.length() == 0 ? "0" : sb.toString();
   }
