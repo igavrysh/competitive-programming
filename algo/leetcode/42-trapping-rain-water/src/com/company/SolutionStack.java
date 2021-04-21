eac8fe0b5a8786635f816aaf12ccc318f7f350cb
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-21 13:37:04 -0700

    temp change

---
+++ algo/leetcode/42-trapping-rain-water/src/com/company/SolutionStack.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Stack;
+
+public class SolutionStack {
+  public int trap(int[] height) {
+    Stack<Integer> s = new Stack<>();
+    for (int i = 0; i < height.length; i++) {
+
+    }
+
+    return 0;
+  }
+}
