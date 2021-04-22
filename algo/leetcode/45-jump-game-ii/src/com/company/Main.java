9035f942fc32caf7783fa13e6eed451bcfe742e3
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-22 06:45:38 -0700

    My solution for problem 45-jump-game-ii

--- algo/leetcode/45-jump-game-ii/src/com/company/Main.java
+++ algo/leetcode/45-jump-game-ii/src/com/company/Main.java
@@ -2,7 +2,33 @@ package com.company;
 
 public class Main {
 
+  public static void test1() {
+    int[] input = {2,3,1,1,4};
+    Solution s = new Solution();
+    int output = s.jump(input);
+    boolean passed = output == 2;
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    int[] input = {2,3,0,1,4};
+    Solution s = new Solution();
+    int output = s.jump(input);
+    boolean passed = output == 2;
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test3() {
+    int[] input = {5,1,1,1,1,1};
+    Solution s = new Solution();
+    int output = s.jump(input);
+    boolean passed = output == 1;
+    System.out.println("test3: " + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
+    test3();
   }
 }
--- algo/leetcode/45-jump-game-ii/src/com/company/Solution.java
+++ algo/leetcode/45-jump-game-ii/src/com/company/Solution.java
@@ -9,15 +9,28 @@ public class Solution {
     for (int i = 0; i < nums.length; i++) {
       r[i] = new ArrayList();
     }
-    int[] DP = new int[nums.length];
+    Integer[] DP = new Integer[nums.length];
     DP[0] = 0;
+    r[0].add(0);
     for (int i = 0; i < nums.length; i++) {
-      for (int j = 1; i <= nums[i]; j++) {
-        r[i+j].add(i);
+      for (int j = 1; j <= nums[i]; j++) {
+        if (i+j < r.length) {
+          r[i + j].add(i);
+        }
+      }
+      Integer min = null;
+      if (i != 0) {
+        for (Integer j : r[i]) {
+          if (DP[j] != null &&
+              (min == null || min > (DP[j] + 1))) {
+            min = DP[j] + 1;
+          }
+        }
+        DP[i] = min;
       }
     }
-
-
-    return 0;
+    return DP[nums.length-1] == null
+        ? -1
+        : DP[nums.length-1].intValue();
   }
 }
