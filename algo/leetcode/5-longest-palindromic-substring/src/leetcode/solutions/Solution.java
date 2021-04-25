c90f3298ada5c530e81f1799d611c74882e58d0f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-25 00:42:35 -0700

    unified leetcode.solutions 5-longest-palindromic-substring problem

--- algo/leetcode/5-longest-palindromic-substring/src/leetcode/solutions/Solution.java
+++ algo/leetcode/5-longest-palindromic-substring/src/leetcode/solutions/Solution.java
@@ -3,18 +3,10 @@ package leetcode.solutions;
 public class Solution {
   public String longestPalindrome(String str) {
     int s = 0, e = 0;
-
     for (int i = 0; i < str.length(); i++) {
-      int l = expand(i, i + 1, str);
-      if (l*2 > e-s) {
-        s=i+1-l;
-        e=i+l;
-      }
-      l = expand(i, i, str);
-      if (l*2-1 > e-s) {
-        s=i+1-l;
-        e=i-1+l;
-      }
+      int l = Math.max(expand(i, i+1, str), expand(i, i, str));
+      s = i-(l-1)/2;
+      e = i+l/2;
     }
     return str.substring(s, e+1);
   }
@@ -24,7 +16,7 @@ public class Solution {
     while (s >= 0 && e < str.length() && str.charAt(s) == str.charAt(e)) {
       s--;
       e++;
-      l++;
+      l += s == e ? 1 : 2;
     }
     return l;
   }
