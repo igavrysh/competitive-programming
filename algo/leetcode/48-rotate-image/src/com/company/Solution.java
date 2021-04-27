ab1d6ef770ab09aa54bb0dac7c1cacb97a9aae34
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-26 23:58:02 -0700

    add definition for problem 48-rotate-image

---
+++ algo/leetcode/48-rotate-image/src/com/company/Solution.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Solution {
+  public void rotate(int[][] matrix) {
+    int N = matrix[0].length;
+
+    int S = (N-1) * 4;
+    for (int s = 0; s < S; s++) {
+      int currS = s;
+      int nextS = -1;
+      for (int i = 0; i < 4; i++) {
+
+      }
+      //int sNxt = s +
+
+
+    }
+  }
+
+  private void nextIJ(int s, int size, int[] currIJ, int[] nextIJ) {
+    //if ()
+  }
+}
