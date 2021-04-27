2b080b959dd992f84e99ac49f5687e5c92820f3a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-27 08:22:58 -0700

    add partial solution for problem 48-rotate-image

--- algo/leetcode/48-rotate-image/src/com/company/Main.java
+++ algo/leetcode/48-rotate-image/src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void test1() {
+    int[][] input = {{1,2,3},{4,5,6},{7,8,9}};
+    Solution s = new Solution();
+    s.rotate(input);
+    boolean passed = Arrays.equals(input[0], new int[]{7,4,1})
+        && Arrays.equals(input[1], new int[]{8,5,2})
+        && Arrays.equals(input[2], new int[]{9,6,3});
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  /*
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+   */
+
+  public static void test2() {
+    int[][] input = {
+        {5, 1,  9,  11},
+        {2, 4,  8,  10},
+        {13,3,  6,  7},
+        {15,14, 12, 16}};
+    Solution s = new Solution();
+    s.rotate(input);
+    boolean passed = Arrays.equals(input[0],  new int[]{15, 13, 2,  5})
+        && Arrays.equals(input[1],            new int[]{14, 3,  4,  1})
+        && Arrays.equals(input[2],            new int[]{12, 6,  8,  9})
+        && Arrays.equals(input[3],            new int[]{16, 7,  10, 11});
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
+
+  public static void main(String[] args) {
+    //test1();
+    test2();
+  }
 }
--- algo/leetcode/48-rotate-image/src/com/company/Solution.java
+++ algo/leetcode/48-rotate-image/src/com/company/Solution.java
@@ -2,22 +2,53 @@ package com.company;
 
 public class Solution {
   public void rotate(int[][] matrix) {
-    int N = matrix[0].length;
-
-    int S = (N-1) * 4;
-    for (int s = 0; s < S; s++) {
-      int currS = s;
-      int nextS = -1;
-      for (int i = 0; i < 4; i++) {
-
+    int N = matrix.length;
+    int s = 0;
+    while (N != 0 && N != 1) {
+      int S = (N - 1) * 4;
+      for (int r = 0; r < N - 1; r++) {
+        int tmp = 0;
+        int previous = 0;
+        for (int i = 0; i < 4; i++) {
+          int[] currIJ = new int[2];
+          int[] nextIJ = new int[2];
+          if (i == 0) {
+            currIJ[0] = s;
+            currIJ[1] = r;
+            nextIJ[0] = r % N;
+            nextIJ[1] = N - 1;
+          }
+          if (i == 1) {
+            currIJ[0] = r % N;
+            currIJ[1] = N - 1;
+            nextIJ[0] = N - 1;
+            nextIJ[1] = N - 1 - r % N;
+          }
+          if (i == 2) {
+            currIJ[0] = N - 1;
+            currIJ[1] = N - 1 - r % N;
+            nextIJ[0] = N - 1 - r % N;
+            nextIJ[1] = s;
+          }
+          if (i == 3) {
+            currIJ[0] = N - 1 - r % N;
+            currIJ[1] = s;
+            nextIJ[0] = s;
+            nextIJ[1] = r;
+          }
+          if (i == 0) {
+            previous = matrix[nextIJ[0]][nextIJ[1]];
+            matrix[nextIJ[0]][nextIJ[1]] = matrix[currIJ[0]][currIJ[1]];
+          } else {
+            tmp = matrix[nextIJ[0]][nextIJ[1]];
+            matrix[nextIJ[0]][nextIJ[1]] = previous;
+            previous = tmp;
+          }
+        }
       }
-      //int sNxt = s +
-
-
+      s += 1;
+      N -= 2;
     }
-  }
 
-  private void nextIJ(int s, int size, int[] currIJ, int[] nextIJ) {
-    //if ()
   }
 }
