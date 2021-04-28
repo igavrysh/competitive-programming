a369f747288a83b778114e8c61da18883851d9e3
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-04-27 21:19:13 -0700

    solution for 48-rotate-image

--- algo/leetcode/48-rotate-image/src/com/company/Solution.java
+++ algo/leetcode/48-rotate-image/src/com/company/Solution.java
@@ -2,53 +2,51 @@ package com.company;
 
 public class Solution {
   public void rotate(int[][] matrix) {
-    int N = matrix.length;
+    int e = matrix.length - 1;
     int s = 0;
-    while (N != 0 && N != 1) {
-      int S = (N - 1) * 4;
-      for (int r = 0; r < N - 1; r++) {
+    while (e>s) {
+      for (int r = 0; r < e-s; r++) {
         int tmp = 0;
         int previous = 0;
         for (int i = 0; i < 4; i++) {
-          int[] currIJ = new int[2];
-          int[] nextIJ = new int[2];
+          int currI = 0, currJ = 0;
+          int nextI = 0, nextJ = 0;
           if (i == 0) {
-            currIJ[0] = s;
-            currIJ[1] = r;
-            nextIJ[0] = r % N;
-            nextIJ[1] = N - 1;
+            currI = s;
+            currJ = s+ r;
+            nextI = s+r;
+            nextJ = e;
           }
           if (i == 1) {
-            currIJ[0] = r % N;
-            currIJ[1] = N - 1;
-            nextIJ[0] = N - 1;
-            nextIJ[1] = N - 1 - r % N;
+            currI = s+r;;
+            currJ = e;
+            nextI = e;
+            nextJ = s+ (e-s-r);
           }
           if (i == 2) {
-            currIJ[0] = N - 1;
-            currIJ[1] = N - 1 - r % N;
-            nextIJ[0] = N - 1 - r % N;
-            nextIJ[1] = s;
+            currI = e;
+            currJ = e - (e-s-r);
+            nextI = s + (e-s -r);
+            nextJ = s;
           }
           if (i == 3) {
-            currIJ[0] = N - 1 - r % N;
-            currIJ[1] = s;
-            nextIJ[0] = s;
-            nextIJ[1] = r;
+            currI = s+(e-s-r);
+            currJ = s;
+            nextI = s;
+            nextJ = s+r;
           }
           if (i == 0) {
-            previous = matrix[nextIJ[0]][nextIJ[1]];
-            matrix[nextIJ[0]][nextIJ[1]] = matrix[currIJ[0]][currIJ[1]];
+            previous = matrix[nextI][nextJ];
+            matrix[nextI][nextJ] = matrix[currI][currJ];
           } else {
-            tmp = matrix[nextIJ[0]][nextIJ[1]];
-            matrix[nextIJ[0]][nextIJ[1]] = previous;
+            tmp = matrix[nextI][nextJ];
+            matrix[nextI][nextJ] = previous;
             previous = tmp;
           }
         }
       }
       s += 1;
-      N -= 2;
+      e -= 1;
     }
-
   }
 }
