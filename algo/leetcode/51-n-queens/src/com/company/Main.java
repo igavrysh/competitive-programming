ff5d084eeb8c958a6f5030ca71cead7075874076
new <new@news-MacBook-Pro.local>
2021-04-29 09:25:56 -0700

    first iteration for problem 51-n-queens

---
+++ algo/leetcode/51-n-queens/src/com/company/Main.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+  private static void test1() {
+    Solution s = new Solution();
+    List<List<String>> output = s.solveNQueens(1);
+    boolean passed = output.size() == 1
+        && output.get(0).size() == 1
+        && output.get(0).get(0).equals("Q");
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  private static void test2() {
+    Solution s = new Solution();
+    List<List<String>> output = s.solveNQueens(2);
+    boolean passed = output.size() == 0;
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
+  private static void test3() {
+    Solution s = new Solution();
+    List<List<String>> output = s.solveNQueens(3);
+    boolean passed = output.size() == 0;
+    System.out.println("test3: " + (passed ? "passed" : "failed"));
+  }
+
+  private static void test4() {
+    Solution s = new Solution();
+    List<List<String>> output = s.solveNQueens(4);
+    boolean passed = output.size() == 0;
+    System.out.println("test3: " + (passed ? "passed" : "failed"));
+  }
+
+  private static void testArraysHash1() {
+    int[] t1 = {99,100,99,100};
+    int hashT1 = Arrays.hashCode(t1);
+
+    int[] t2 = {99,100,99,100};
+    int hashT2 = Arrays.hashCode(t2);
+
+    System.out.println("testArraysHash1: hashT1 == hashT2 ? " + (hashT1 == hashT2 ? "true" : "false") );
+  }
+
+  private static void testArraysHash2() {
+    int[][] t1 = {{99,100,99,100}, {99,99,100,100}, {100, 99, 100, 99}, {100, 100, 100, 100}};
+    int hashT1 = Arrays.deepHashCode(t1);
+
+    int[][] t2 = {{99,100,99,100}, {99,99,100,100}, {100, 99, 100, 99}, {100, 100, 100, 100}};
+    int hashT2 = Arrays.deepHashCode(t2);
+
+    System.out.println("testArraysHash2: hashT1 == hashT2 ? " + (hashT1 == hashT2 ? "true" : "false") );
+  }
+
+  public static void main(String[] args) {
+    test1();
+    test2();
+    test3();
+    test4();
+    testArraysHash1();
+    testArraysHash2();
+  }
+}
