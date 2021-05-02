fb53f0f1c278e61c4b8106bcb6595c4b40cb498a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-05-01 19:48:06 -0700

    solution for 56-merge-intervals problem

--- algo/leetcode/56-merge-intervals/src/com/company/Main.java
+++ algo/leetcode/56-merge-intervals/src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
+  public static void test1() {
+    int[][] input = {{1,3},{2,6},{8,10},{15,18}};
+    Solution s = new Solution();
+    int[][] output = s.merge(input);
+    boolean passed = output.length == 3
+       && Arrays.deepEquals(output, new int[][] {{1,6},{8,10},{15,18}});
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    int[][] input = {{1,4},{4,5}};
+    Solution s = new Solution();
+    int[][] output = s.merge(input);
+    boolean passed = output.length == 1
+        && Arrays.deepEquals(output, new int[][] {{1,5}});
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
   }
 }
--- algo/leetcode/56-merge-intervals/src/com/company/Solution.java
+++ algo/leetcode/56-merge-intervals/src/com/company/Solution.java
@@ -4,13 +4,29 @@ import java.util.Arrays;
 
 public class Solution {
   public int[][] merge(int[][] intervals) {
-    return new int[0][0];
+    Arrays.sort(intervals, (int1, int2) -> { return int1[0] - int2[0]; });
+    int l = intervals[0][0];
+    int r = intervals[0][1];
+    int k = 0;
+    for (int i = 1; i < intervals.length; i++) {
+      if (intervals[i][0] > r) {
+        intervals[k][0] = l;
+        intervals[k][1] = r;
+        k++;
 
-    Arrays.sort(intervals, (int[] int1, int[] int2) -> { return int1[0] - int2[0]; });
-
-
-
-    return new int[0][0];
-
+        l = intervals[i][0];
+        r = intervals[i][1];
+      } else if (intervals[i][0] <= r && intervals[i][1] > r) {
+        r = intervals[i][1];
+      }
+    }
+    intervals[k][0] = l;
+    intervals[k][1] = r;
+    k++;
+    int[][] output = new int[k][2];
+    for (int i = 0; i < k; i++) {
+      output[i] =  intervals[i];
+    }
+    return output;
   }
 }
