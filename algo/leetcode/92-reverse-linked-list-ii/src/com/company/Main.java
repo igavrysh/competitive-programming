8fb61f03b756b64be071a1c7c3393986d7b50a5f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-05-08 15:46:17 -0700

    solution for problem 92-reverse-linked-list-ii

--- algo/leetcode/92-reverse-linked-list-ii/src/com/company/Main.java
+++ algo/leetcode/92-reverse-linked-list-ii/src/com/company/Main.java
@@ -2,7 +2,47 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void test1() {
+    ListNode input = new ListNode(
+        1,
+        new ListNode(
+            2,
+            new ListNode(
+                3,
+                new ListNode(
+                    4,
+                    new ListNode(
+                        5,
+                        null
+                    )
+                )
+            )
+        )
+    );
+    Solution s = new Solution();
+    ListNode output = s.reverseBetween(input, 2, 4);
+    boolean passed = output.val == 1
+        && output.next.val == 4
+        && output.next.next.val == 3
+        && output.next.next.next.val == 2
+        && output.next.next.next.next.val == 5;
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    ListNode input = new ListNode(
+        5,
+        null
+    );
+    Solution s = new Solution();
+    ListNode output = s.reverseBetween(input, 1, 1);
+    boolean passed = output.val == 5
+        && output.next == null;
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void main(String[] args) {
+    test1();
+    test2();
+  }
 }
--- algo/leetcode/92-reverse-linked-list-ii/src/com/company/Solution.java
+++ algo/leetcode/92-reverse-linked-list-ii/src/com/company/Solution.java
@@ -2,7 +2,35 @@ package com.company;
 
 public class Solution {
   public ListNode reverseBetween(ListNode head, int left, int right) {
-
+    ListNode proxyHead = new ListNode(0, head);
+    int i = -1;
+    ListNode prevStart = null;
+    ListNode startN = null;
+    ListNode endN = null;
+    ListNode c = proxyHead;
+    ListNode n = c.next;
+    ListNode nn = c.next.next;
+    while (c != null && i <= right-1) {
+      if (i == left-2) {
+        prevStart = c;
+      }
+      if (i == left-1) {
+        startN = c;
+      }
+      if (i == right-1) {
+        endN = c;
+      }
+      ListNode tmp = nn != null ? nn.next : null;
+      if (i >= left-1 && i < right-1) {
+        n.next = c;
+      }
+      c = n;
+      n = nn;
+      nn = tmp;
+      i++;
+    }
+    prevStart.next = endN;
+    startN.next = c;
+    return proxyHead.next;
   }
-
 }
