29ff3e80400e8046ff71fa7d7064068fed5fd67e
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-05-23 12:20:15 -0700

    plan for O(N) solution for problem 75-sort-colors

---
+++ algo/leetcode/75-sort-colors/src/com/company/Solution3.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Solution3 {
+  public void sortColors(int[] nums) {
+
+    // Unknown area = Mid..Hi
+    // ZeroesArea = 0..Low-1
+    // OnesArea = Low..Mid-1
+    // TwosArea = Hi+1..N-1
+
+    int low = 0;
+    int mid = 0;
+    int hi = nums.length-1;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == 0) {
+
+      }
+    }
+
+
+
+
+  }
+
+}
