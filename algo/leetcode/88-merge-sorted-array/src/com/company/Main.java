72714875c8fd6fb6e8817882f18592572f5b8112
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-07-12 01:25:45 -0700

    optimized solution for problem 88-merge-sorted-array

--- algo/leetcode/88-merge-sorted-array/src/com/company/Main.java
+++ algo/leetcode/88-merge-sorted-array/src/com/company/Main.java
@@ -37,9 +37,46 @@ public class Main {
     System.out.println("test3: " + (passed ? "passed" : "failed"));
   }
 
+  public static void test11() {;
+    int[] nums1 = {1,2,3,0,0,0};
+    int m = 3;
+    int[] nums2 = {2,5,6};
+    int n = 3;
+    SolutionOptimized s = new SolutionOptimized();
+    s.merge(nums1, m, nums2, n);
+    boolean passed = Arrays.equals(nums1, new int[] {1,2,2,3,5,6});
+    System.out.println("test11: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test12() {
+    int[] nums1 = {1};
+    int m = 1;
+    int[] nums2 = {};
+    int n = 0;
+    SolutionOptimized s = new SolutionOptimized();
+    s.merge(nums1, m, nums2, n);
+    boolean passed = Arrays.equals(nums1, new int[] {1});
+    System.out.println("test12: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test13() {
+    int[] nums1 = {0};
+    int m = 0;
+    int[] nums2 = {1};
+    int n = 1;
+    SolutionOptimized s = new SolutionOptimized();
+    s.merge(nums1, m, nums2, n);
+    boolean passed = Arrays.equals(nums1, new int[] {1});
+    System.out.println("test13: " + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
     test1();
     test2();
     test3();
+
+    test11();
+    test12();
+    test13();
   }
 }
---
+++ algo/leetcode/88-merge-sorted-array/src/com/company/SolutionOptimized.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class SolutionOptimized {
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int i = m-1;
+    int j = n-1;
+    for (int s = n+m-1; s >= 0; s--) {
+      if (j < 0 && i >= 0) {
+        nums1[s] = nums1[i];
+        i--;
+      } else if (i < 0 && j >= 0) {
+        nums1[s] = nums2[j];
+        j--;
+      } else if (i >= 0 && j >= 0) {
+        if (nums1[i] >= nums2[j]) {
+          nums1[s] = nums1[i];
+          i--;
+        } else {
+          nums1[s] = nums2[j];
+          j--;
+        }
+      }
+    }
+  }
+}
