ac7c466ba73ac03c06ecd0d43e3475fde1e6ac4c
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-08-12 17:39:52 -0700

    get access tokens

--- src/main/java/com/gotarrays/userservice/security/SecurityConfig.java
+++ src/main/java/com/gotarrays/userservice/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.gotarrays.userservice.security;
 
+import com.gotarrays.userservice.filter.CustomerAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -24,6 +28,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    super.configure(http);
+    http.csrf().disable();
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.authorizeRequests().anyRequest().permitAll();
+    http.addFilter(new CustomerAuthenticationFilter(authenticationManagerBean()));
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 }
