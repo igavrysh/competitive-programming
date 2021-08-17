329285e96e7451afc8859e1526464cc86a45b28a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-08-17 16:51:21 -0700

    refresh token feature

--- src/main/java/com/gotarrays/userservice/api/UserResource.java
+++ src/main/java/com/gotarrays/userservice/api/UserResource.java
@@ -1,13 +1,36 @@
 package com.gotarrays.userservice.api;
 
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gotarrays.userservice.domain.Role;
 import com.gotarrays.userservice.domain.User;
 import com.gotarrays.userservice.service.UserService;
+import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,6 +66,42 @@ public class UserResource {
     userService.addRoleToUser(form.getUserName(), form.getRoleName());
     return ResponseEntity.ok().build();
   }
+
+  @GetMapping("/token/refresh")
+  public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String authorizationHeader = request.getHeader(AUTHORIZATION);
+    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+      try {
+        String refreshToken = authorizationHeader.substring("Bearer ".length());
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT decodedJWT = verifier.verify(refreshToken);
+        String username = decodedJWT.getSubject();
+        User user = userService.getUser(username);
+        String access_token = JWT.create()
+            .withSubject(user.getUsername())
+            .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))  // 10 mins
+            .withIssuer(request.getRequestURL().toString())
+            .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
+            .sign(algorithm);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refreshToken);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+      } catch (Exception exception) {
+        response.setHeader("error", exception.getMessage());
+        response.setStatus(FORBIDDEN.value());
+        //response.sendError(FORBIDDEN.value());
+        Map<String, String> error = new HashMap<>();
+        error.put("error_message", exception.getMessage());
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), error);
+      }
+    } else {
+      throw new RuntimeException("Refresh token is missing");
+    }
+  }
 }
 
 @Data
@@ -50,3 +109,4 @@ class RoleToUserForm {
   private String userName;
   private String roleName;
 }
+
