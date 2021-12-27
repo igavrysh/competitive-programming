439169329f8816f155e8deab2d26dd8ebfe68306
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-12-26 21:48:17 -0800

    quickselect solution for problem 973 k closest points to origin

--- algo/leetcode/973-k-closest-points-to-origin/src/com/company/Main.java
+++ algo/leetcode/973-k-closest-points-to-origin/src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
+  public static void test1() {
+    int[][] input = {{1,3},{-2,2}};
+    int k = 1;
+    Solution s = new Solution();
+    int[][] output = s.kClosest(input, k);
+    boolean passed = output.length == 1 && Arrays.equals(output[0], new int[]{-2,2});
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    int[][] input = {{3,3},{5,-1},{-2,4}};
+    int k = 2;
+    Solution s = new Solution();
+    int[][] output = s.kClosest(input, k);
+    boolean passed = output.length == 2 && Arrays.deepEquals(output, new int[][]{{3,3}, {-2,4}});
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
   }
 }
--- algo/leetcode/973-k-closest-points-to-origin/src/com/company/Solution.java
+++ algo/leetcode/973-k-closest-points-to-origin/src/com/company/Solution.java
@@ -1,10 +1,68 @@
 package com.company;
 
-public class Solution {
-  public int[][] kClosest(int[][] points, int k) {
-    for (int i = 0; i < points.length; i++) {
+import java.util.Random;
 
+public class Solution {
+  private void swap(int i, int j, int [][]points) {
+    int[] tmp = points[i];
+    points[i] = points[j];
+    points[j] = tmp;
+  }
+
+  public int[][] kClosest(int[][] points, int k) {
+    quickSelect(k, 0, points.length-1, points);
+    int[][] output = new int[k][2];
+    for (int i = 0; i < k; i++) {
+      output[i] = points[i];
+    }
+    return output;
+  }
+
+  private int partition(int l, int r, int pivot, int[][] points) {
+    if (l == r) {
+      return l;
     }
 
+    double distPivot = dist(points[pivot]);
+
+    // move pivot to the right
+    swap(pivot, r, points);
+
+    // move smaller points to the left
+    int storeIndex = l;
+    for (int i = l; i < r; i++) {
+      if (dist(points[i]) < distPivot) {
+        swap(storeIndex, i, points);
+        storeIndex++;
+      }
+    }
+
+    // put pivot to its final place
+    swap(r, storeIndex, points);
+    return storeIndex;
   }
+
+  private void quickSelect(int k, int l, int r, int[][] points) {
+    Random rand = new Random();
+
+    int pivotIdx = l + rand.nextInt(r-l+1);
+
+    int pos = partition(l, r, pivotIdx, points);
+
+    if (pos == k-1) {
+      return;
+    } else if (pos > k-1) {
+      r = pos-1;
+      quickSelect(k, l, r, points);
+    } else {
+      l = pos+1;
+      quickSelect(k, l, r, points);
+    }
+  }
+
+  private double dist(int[] point) {
+    return Math.pow(point[0], 2) + Math.pow(point[1], 2);
+  }
+
+
 }
