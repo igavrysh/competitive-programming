9a587fe50233855ce7afebbd3a5e83a2eca53c28
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-12-27 04:40:30 -0800

    solution for problem 1010 pairs of songs with total durations divisible by 60

--- algo/leetcode/1010-pairs-of-songs-with-total-durations-divisible-by-60/src/com/company/Solution.java
+++ algo/leetcode/1010-pairs-of-songs-with-total-durations-divisible-by-60/src/com/company/Solution.java
@@ -5,15 +5,24 @@ public class Solution {
     int[] fq = new int[60];
     int output = 0;
     for (int i = 0; i < time.length; i++) {
-      int compl = 60 - (time[i] % 60);
-      if (compl == 60) {
-        compl = 0;
-      }
-      if (fq[compl] == 0) {
-        fq[time[i]%60] = 1;
-      } else {
-        output+= fq[compl];
-        fq[time[i]%60] += 1;
+      fq[time[i]%60] += 1;
+    }
+
+    for (int i = 0; i < fq.length; i++) {
+      if (fq[i] != 0) {
+        int compl = (60 - i)%60;
+
+        // number of combinations pairs out of fq[i]
+        if (i == compl) {
+          output += fq[i] * (fq[i] - 1) / 2;
+          fq[compl] = 0;
+          continue;
+        }
+
+        // othewise simple multiplication
+        output += fq[i] * fq[compl];
+        fq[i] = 0;
+        fq[compl] = 0;
       }
     }
     return output;
