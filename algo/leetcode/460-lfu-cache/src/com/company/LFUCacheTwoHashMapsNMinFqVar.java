61b9055f5ffe5e11dbc0ac850fd510232cf3b18f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-12-28 13:26:51 -0800

    add two hash map + min fq var for 460 lfu cache problem

---
+++ algo/leetcode/460-lfu-cache/src/com/company/LFUCacheTwoHashMapsNMinFqVar.java
@@ -0,0 +1,146 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class LFUCacheTwoHashMapsNMinFqVar {
+  public HashMap<Integer, DLinkedList> fq;
+  public HashMap<Integer, Node> nodes;
+  public int capacity;
+  public int currSize;
+  public int minFQ;
+
+  public LFUCacheTwoHashMapsNMinFqVar(int capacity) {
+    this.capacity = capacity;
+    currSize = 0;
+    minFQ = -1;
+    nodes = new HashMap<>();
+    fq = new HashMap<>();
+  }
+
+  public int get(int key) {
+    if (nodes.get(key) == null) {
+      return -1;
+    }
+    Node node = nodes.get(key);
+    return setAndGet(key, node.value);
+  }
+
+  private int setAndGet(int key, int value) {
+    if (nodes.get(key) == null) {
+      return -1;
+    }
+    Node node = nodes.get(key);
+    node.value = value;
+    DLinkedList l = fq.get(node.useCounter);
+    fq.get(node.useCounter).remove(node);
+    if (l.getSize() == 0) {
+      fq.remove(node.useCounter);
+      if (node.useCounter == minFQ) {
+        minFQ++;
+      }
+    }
+    node.useCounter++;
+    DLinkedList newL = fq.getOrDefault(node.useCounter, new DLinkedList());
+    fq.put(node.useCounter, newL);
+    newL.addFirst(node);
+    return node.value;
+  }
+
+  // removeLeastUsed removes least used element without updating minFq variable
+  private Node removeLeastUsed() {
+    if (currSize == 0) {
+      return null;
+    }
+    DLinkedList l = fq.get(minFQ);
+    Node node = l.removeLast();
+    if (l.getSize() == 0) {
+      fq.remove(minFQ);
+    }
+    currSize--;
+    return node;
+  }
+
+  public void put(int key, int value) {
+    if (capacity == 0) {
+      return;
+    }
+    if (nodes.get(key) != null) {
+      setAndGet(key, value);
+      return;
+    }
+    Node node = new Node(key, value);
+    node.useCounter = 1;
+    nodes.put(key, node);
+
+    if (currSize == capacity) {
+      Node removedNode = removeLeastUsed();
+      nodes.remove(removedNode.key);
+    }
+
+    DLinkedList l = fq.getOrDefault(1, new DLinkedList());
+    l.addFirst(node);
+    fq.put(1, l);
+    minFQ = 1;
+    currSize++;
+  }
+
+  class Node {
+    int key;
+    int value;
+    Node next;
+    Node prev;
+    int useCounter = 0;
+
+    Node(int key, int value) {
+      this.key = key;
+      this.value = value;
+      useCounter = 1;
+    }
+  }
+
+  class DLinkedList {
+    Node sentinel;
+    int size;
+
+    public DLinkedList() {
+      sentinel = new Node(Integer.MIN_VALUE, Integer.MIN_VALUE);
+      sentinel.next = sentinel;
+      sentinel.prev = sentinel;
+      size = 0;
+    }
+
+    public int getSize() {
+      return size;
+    }
+
+    public void addFirst(Node node) {
+      Node sentinelNext = sentinel.next;
+      sentinel.next = node;
+      node.prev = sentinel;
+      node.next = sentinelNext;
+      sentinelNext.prev = node;
+      size++;
+    }
+
+    public void remove(Node node) {
+      Node nodePrev = node.prev;
+      Node nodeNext = node.next;
+      nodePrev.next = nodeNext;
+      nodeNext.prev = nodePrev;
+      size--;
+    }
+
+    public Node removeLast() {
+      if (size == 0) {
+        return null;
+      }
+      Node node = sentinel.prev;
+      node.prev.next = sentinel;
+      sentinel.prev = node.prev;
+      size--;
+      node.next = null;
+      node.prev = null;
+      return node;
+    }
+  }
+}
--- algo/leetcode/460-lfu-cache/src/com/company/Main.java
+++ algo/leetcode/460-lfu-cache/src/com/company/Main.java
@@ -11,167 +11,209 @@ public class Main {
   public static void test1() {
     boolean passed = true;
 
-    LFUCache cache = new LFUCache(2);
+    LFUCacheTwoHashMapsNMinFqVar cache = new LFUCacheTwoHashMapsNMinFqVar(2);
     passed = passed
-        && cache.k == 2
-        && cache.dict != null
-        && cache.head == null
-        && cache.tail == null;
+        && cache.capacity == 2
+        && cache.nodes != null
+        && cache.fq != null
+        && cache.currSize == 0;
     System.out.println(getMethodName() + " - cache creation: " + (passed ? "passed" : "failed"));
+
 
     cache.put(1, 1);
     passed = passed
-        && cache.dict.get(1).value == 1
-        && cache.dict.get(1).useCounter == 1
-        && cache.head.value == 1
-        && cache.tail.value == 1
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.nodes.size() == 1
+        && cache.fq.size() == 1
+        && cache.nodes.get(1).value == 1
+        && cache.nodes.get(1).useCounter == 1
+        && cache.fq.get(1).getSize() == 1
+        && cache.fq.get(1).sentinel.next.prev == cache.fq.get(1).sentinel
+        && cache.fq.get(1).sentinel.next.next == cache.fq.get(1).sentinel
+        && cache.fq.get(1).sentinel.next.value == 1;
     System.out.println(getMethodName() + " - inserting first element: " + (passed ? "passed" : "failed"));
+
 
     cache.put(2,2);
     passed = passed
-        && cache.dict.size() == 2
-        && cache.dict.get(1).value == 1
-        && cache.dict.get(1).useCounter == 1
-        && cache.dict.get(2).value == 2
-        && cache.dict.get(2).useCounter == 1
-        && cache.dict.get(1).prev == null
-        && cache.dict.get(1).next.equals(cache.dict.get(2))
-        && cache.dict.get(2).prev.equals(cache.dict.get(1))
-        && cache.dict.get(2).next == null
-        && cache.head.equals(cache.dict.get(1))
-        && cache.tail.equals(cache.dict.get(2))
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.minFQ == 1
+        && cache.capacity == 2
+        && cache.currSize == 2
+        && cache.nodes.size() == 2
+        && cache.fq.size() == 1
+        && cache.nodes.get(1).value == 1
+        && cache.nodes.get(1).useCounter == 1
+        && cache.nodes.get(2).value == 2
+        && cache.nodes.get(2).useCounter == 1
+        && cache.nodes.get(1).prev == cache.nodes.get(2)
+        && cache.nodes.get(1).next == cache.fq.get(1).sentinel
+        && cache.nodes.get(2).prev == cache.fq.get(1).sentinel
+        && cache.nodes.get(2).next == cache.nodes.get(1);
     System.out.println(getMethodName() + " - inserting second element: " + (passed ? "passed" : "failed"));
+
 
     int val = cache.get(1);
     passed = passed
         && val == 1
-        && cache.dict.size() == 2
-        && cache.dict.get(1).value == 1
-        && cache.dict.get(1).useCounter == 2
-        && cache.dict.get(2).value == 2
-        && cache.dict.get(2).useCounter == 1
-        && cache.dict.get(2).prev == null
-        && cache.dict.get(2).next.equals(cache.dict.get(1))
-        && cache.dict.get(1).prev.equals(cache.dict.get(2))
-        && cache.dict.get(1).next == null
-        && cache.head.equals(cache.dict.get(2))
-        && cache.tail.equals(cache.dict.get(1))
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.minFQ == 1
+        && cache.capacity == 2
+        && cache.currSize == 2
+        && cache.nodes.size() == 2
+        && cache.nodes.get(1).value == 1
+        && cache.nodes.get(1).useCounter == 2
+        && cache.nodes.get(2).value == 2
+        && cache.nodes.get(2).useCounter == 1
+        && cache.fq.size() == 2
+        && cache.fq.get(1).size == 1
+        && cache.fq.get(2).size == 1
+        && cache.nodes.get(1).next == cache.fq.get(2).sentinel
+        && cache.nodes.get(1).prev == cache.fq.get(2).sentinel
+        && cache.fq.get(2).sentinel.next == cache.nodes.get(1)
+        && cache.fq.get(2).sentinel.prev == cache.nodes.get(1)
+        && cache.nodes.get(2).next == cache.fq.get(1).sentinel
+        && cache.nodes.get(2).prev == cache.fq.get(1).sentinel
+        && cache.fq.get(1).sentinel.next == cache.nodes.get(2)
+        && cache.fq.get(1).sentinel.prev == cache.nodes.get(2);
+
     System.out.println(getMethodName() + " - getting element 1 - should rearrange list: " + (passed ? "passed" : "failed"));
 
     val = cache.get(1);
     passed = passed
         && val == 1
-        && cache.dict.size() == 2
-        && cache.dict.get(1).value == 1
-        && cache.dict.get(1).useCounter == 3
-        && cache.dict.get(2).value == 2
-        && cache.dict.get(2).useCounter == 1
-        && cache.dict.get(2).prev == null
-        && cache.dict.get(2).next.equals(cache.dict.get(1))
-        && cache.dict.get(1).prev.equals(cache.dict.get(2))
-        && cache.dict.get(1).next == null
-        && cache.head.equals(cache.dict.get(2))
-        && cache.tail.equals(cache.dict.get(1))
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.minFQ == 1
+        && cache.capacity == 2
+        && cache.currSize == 2
+        && cache.nodes.size() == 2
+        && cache.nodes.get(1).value == 1
+        && cache.nodes.get(1).useCounter == 3
+        && cache.nodes.get(2).value == 2
+        && cache.nodes.get(2).useCounter == 1
+        && cache.fq.size() == 2
+        && cache.fq.get(1).size == 1
+        && cache.fq.get(3).size == 1
+        && cache.nodes.get(1).next == cache.fq.get(3).sentinel
+        && cache.nodes.get(1).prev == cache.fq.get(3).sentinel
+        && cache.fq.get(3).sentinel.next == cache.nodes.get(1)
+        && cache.fq.get(3).sentinel.prev == cache.nodes.get(1)
+        && cache.nodes.get(2).next == cache.fq.get(1).sentinel
+        && cache.nodes.get(2).prev == cache.fq.get(1).sentinel
+        && cache.fq.get(1).sentinel.next == cache.nodes.get(2)
+        && cache.fq.get(1).sentinel.prev == cache.nodes.get(2);
     System.out.println(getMethodName() + " - getting element 1 again - no rearrangments are expected: " + (passed ? "passed" : "failed"));
 
     cache.put(3, 3);
     passed = passed
-        && cache.dict.size() == 2
-        && cache.dict.get(1).value == 1
-        && cache.dict.get(1).useCounter == 3
-        && cache.dict.get(2) == null
-        && cache.dict.get(3).value == 3
-        && cache.dict.get(3).useCounter == 1
-        && cache.dict.get(3).prev == null
-        && cache.dict.get(3).next.equals(cache.dict.get(1))
-        && cache.dict.get(1).prev.equals(cache.dict.get(3))
-        && cache.dict.get(1).next == null
-        && cache.head.equals(cache.dict.get(3))
-        && cache.tail.equals(cache.dict.get(1))
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.nodes.size() == 2
+        && cache.minFQ == 1
+        && cache.capacity == 2
+        && cache.currSize == 2
+        && cache.nodes.get(1).value == 1
+        && cache.nodes.get(1).useCounter == 3
+        && cache.nodes.get(2) == null
+        && cache.nodes.get(3).value == 3
+        && cache.nodes.get(3).useCounter == 1
+        && cache.fq.size() == 2
+        && cache.fq.get(1).size == 1
+        && cache.fq.get(3).size == 1
+
+        && cache.nodes.get(1).next == cache.fq.get(3).sentinel
+        && cache.nodes.get(1).prev == cache.fq.get(3).sentinel
+        && cache.fq.get(3).sentinel.next == cache.nodes.get(1)
+        && cache.fq.get(3).sentinel.prev == cache.nodes.get(1)
+        && cache.nodes.get(3).next == cache.fq.get(1).sentinel
+        && cache.nodes.get(3).prev == cache.fq.get(1).sentinel
+        && cache.fq.get(1).sentinel.next == cache.nodes.get(3)
+        && cache.fq.get(1).sentinel.prev == cache.nodes.get(3);
     System.out.println(getMethodName() + " - inserting element (3,3): " + (passed ? "passed" : "failed"));
   }
 
   public static void test2() {
     boolean passed = true;
 
-    LFUCache cache = new LFUCache(1);
+    LFUCacheTwoHashMapsNMinFqVar cache = new LFUCacheTwoHashMapsNMinFqVar(1);
     passed = passed
-        && cache.k == 1
-        && cache.dict != null
-        && cache.head == null
-        && cache.tail == null;
+        && cache.currSize == 0
+        && cache.capacity == 1
+        && cache.nodes != null
+        && cache.nodes.size() == 0
+        && cache.fq != null
+        && cache.fq.size() == 0;
     System.out.println(getMethodName() + " - cache creation: " + (passed ? "passed" : "failed"));
 
     cache.put(1, 1);
     passed = passed
-        && cache.dict.get(1).value == 1
-        && cache.dict.get(1).useCounter == 1
-        && cache.head.value == 1
-        && cache.tail.value == 1
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.nodes.get(1).value == 1
+        && cache.nodes.get(1).useCounter == 1
+        && cache.minFQ == 1
+        && cache.capacity == 1
+        && cache.currSize == 1
+        && cache.nodes.size() == 1
+        && cache.nodes.get(1).value == 1
+        && cache.nodes.get(1).useCounter == 1
+        && cache.fq.size() == 1
+        && cache.fq.get(1).size == 1
+        && cache.nodes.get(1).next == cache.fq.get(1).sentinel
+        && cache.nodes.get(1).prev == cache.fq.get(1).sentinel
+        && cache.fq.get(1).sentinel.next == cache.nodes.get(1)
+        && cache.fq.get(1).sentinel.prev == cache.nodes.get(1);
     System.out.println(getMethodName() + " - inserting first element: " + (passed ? "passed" : "failed"));
 
     cache.put(2,2);
     passed = passed
-        && cache.dict.size() == 1
-        && cache.dict.get(1) == null
-        && cache.dict.get(2).value == 2
-        && cache.dict.get(2).useCounter == 1
-        && cache.dict.get(2).prev == null
-        && cache.dict.get(2).next == null
-        && cache.head.equals(cache.dict.get(2))
-        && cache.tail.equals(cache.dict.get(2))
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.nodes.get(2).value == 2
+        && cache.nodes.get(2).useCounter == 1
+        && cache.minFQ == 1
+        && cache.capacity == 1
+        && cache.currSize == 1
+        && cache.nodes.size() == 1
+        && cache.fq.size() == 1
+        && cache.fq.get(1).size == 1
+        && cache.nodes.get(2).next == cache.fq.get(1).sentinel
+        && cache.nodes.get(2).prev == cache.fq.get(1).sentinel
+        && cache.fq.get(1).sentinel.next == cache.nodes.get(2)
+        && cache.fq.get(1).sentinel.prev == cache.nodes.get(2);
     System.out.println(getMethodName() + " - inserting second element: " + (passed ? "passed" : "failed"));
+
 
     int val = cache.get(2);
     passed = passed
         && val == 2
-        && cache.dict.size() == 1
-        && cache.dict.get(2).value == 2
-        && cache.dict.get(2).useCounter == 2
-        && cache.dict.get(2).prev == null
-        && cache.dict.get(2).next == null
-        && cache.head.equals(cache.dict.get(2))
-        && cache.tail.equals(cache.dict.get(2))
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.nodes.get(2).value == 2
+        && cache.nodes.get(2).useCounter == 2
+        && cache.minFQ == 2
+        && cache.capacity == 1
+        && cache.currSize == 1
+        && cache.nodes.size() == 1
+        && cache.fq.size() == 1
+        && cache.fq.get(2).size == 1
+        && cache.nodes.get(2).next == cache.fq.get(2).sentinel
+        && cache.nodes.get(2).prev == cache.fq.get(2).sentinel
+        && cache.fq.get(2).sentinel.next == cache.nodes.get(2)
+        && cache.fq.get(2).sentinel.prev == cache.nodes.get(2);
     System.out.println(getMethodName() + " - getting element 2 - should rearrange list: " + (passed ? "passed" : "failed"));
 
     cache.put(3, 3);
     passed = passed
-        && cache.dict.size() == 1
-        && cache.dict.get(3).value == 3
-        && cache.dict.get(3).useCounter == 1
-        && cache.dict.get(3).prev == null
-        && cache.dict.get(3).next == null
-        && cache.head.equals(cache.dict.get(3))
-        && cache.tail.equals(cache.dict.get(3))
-        && cache.head.prev == null
-        && cache.tail.next == null;
+        && cache.nodes.get(3).value == 3
+        && cache.nodes.get(3).useCounter == 1
+        && cache.minFQ == 1
+        && cache.capacity == 1
+        && cache.currSize == 1
+        && cache.nodes.size() == 1
+        && cache.fq.size() == 1
+        && cache.fq.get(1).size == 1
+        && cache.nodes.get(3).next == cache.fq.get(1).sentinel
+        && cache.nodes.get(3).prev == cache.fq.get(1).sentinel
+        && cache.fq.get(1).sentinel.next == cache.nodes.get(3)
+        && cache.fq.get(1).sentinel.prev == cache.nodes.get(3);
     System.out.println(getMethodName() + " - inserting element (3,3): " + (passed ? "passed" : "failed"));
   }
 
   public static void test3() {
     boolean passed = true;
-
-    LFUCache cache = new LFUCache(0);
+    LFUCacheTwoHashMapsNMinFqVar cache = new LFUCacheTwoHashMapsNMinFqVar(0);
     cache.put(0, 0);
     passed = passed && cache.get(0) == -1;
     System.out.println(getMethodName() + ": " + (passed ? "passed" : "failed"));
-
   }
 
   public static void main(String[] args) {
