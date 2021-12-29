0acf13e4b1d4ba966549f6d138f57d3b206ce7c2
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-12-28 22:04:12 -0800

    add problem statement for 1152 analyse user website visit pattern

---
+++ algo/leetcode/1152-analyze-user-website-visit-pattern/src/com/company/Solution.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.List;
+
+public class Solution {
+  public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
+    return null;
+  }
+
+}
