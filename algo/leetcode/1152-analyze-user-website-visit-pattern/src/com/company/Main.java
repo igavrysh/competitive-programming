73e929020f5f13a7d1e1eba7af276e2c74e74521
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-12-29 01:25:18 -0800

    solution for problem 1152 analyze user website visit pattern

--- algo/leetcode/1152-analyze-user-website-visit-pattern/src/com/company/Main.java
+++ algo/leetcode/1152-analyze-user-website-visit-pattern/src/com/company/Main.java
@@ -1,8 +1,65 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
+  public static void test1() {
+    String[] username = {"joe","joe","joe","james","james","james","james","mary","mary","mary"};
+    int[] timestamp = {1,2,3,4,5,6,7,8,9,10};
+    String[] website = {"home","about","career","home","cart","maps","home","home","about","career"};
+    Solution s = new Solution();
+    List<String> output = s.mostVisitedPattern(username, timestamp, website);
+    boolean passed = Arrays.equals(output.toArray(), new String[]{"home","about","career"});
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    String[] username = {"ua","ua","ua","ub","ub","ub"};
+    int[] timestamp = {1,2,3,4,5,6};
+    String[] website = {"a","b","a","a","b","c"};
+    Solution s = new Solution();
+    List<String> output = s.mostVisitedPattern(username, timestamp, website);
+    boolean passed = Arrays.equals(output.toArray(), new String[]{"a","b","a"});
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test3() {
+    String[] username = {"dowg","dowg","dowg"};
+    int[] timestamp = {158931262,562600350,148438945};
+    String[] website = {"y","loedo","y"};
+    Solution s = new Solution();
+    List<String> output = s.mostVisitedPattern(username, timestamp, website);
+    boolean passed = Arrays.equals(output.toArray(), new String[]{"y","y","loedo"});
+    System.out.println("test3: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test4() {
+    String[] username = {"pdtkrjhd","pdtkrjhd","pdtkrjhd","pdtkrjhd","pdtkrjhd","pdtkrjhd"};
+    int[] timestamp = {210984153,262799291,958396687,605779010,373702273,205190519};
+    String[] website = {"xgriygexlk","qs","rugydl","bkrok","canlv","cahgsobjjs"};
+    Solution s = new Solution();
+    List<String> output = s.mostVisitedPattern(username, timestamp, website);
+    boolean passed = Arrays.equals(output.toArray(), new String[]{"cahgsobjjs","bkrok","rugydl"});
+    System.out.println("test4: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test5() {
+    String[] username = {"h","eiy","cq","h","cq","txldsscx","cq","txldsscx","h","cq","cq"};
+    int[] timestamp = {527896567,334462937,517687281,134127993,859112386,159548699,51100299,444082139,926837079,317455832,411747930};
+    String[] website = {"hibympufi","hibympufi","hibympufi","hibympufi","hibympufi","hibympufi","hibympufi","hibympufi","yljmntrclw","hibympufi","yljmntrclw"};
+    Solution s = new Solution();
+    List<String> output = s.mostVisitedPattern(username, timestamp, website);
+    boolean passed = Arrays.equals(output.toArray(), new String[]{"hibympufi","hibympufi","yljmntrclw"});
+    System.out.println("test5: " + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
   }
 }
--- algo/leetcode/1152-analyze-user-website-visit-pattern/src/com/company/Solution.java
+++ algo/leetcode/1152-analyze-user-website-visit-pattern/src/com/company/Solution.java
@@ -1,10 +1,64 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import javafx.util.Pair;
 
 public class Solution {
   public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
-    return null;
-  }
+    HashMap<String, ArrayList<Pair<Integer, String>>> H = new HashMap<>();
 
+    for (int i = 0; i < username.length; i++) {
+      ArrayList<Pair<Integer, String>> l = H.getOrDefault(username[i], new ArrayList<>());
+      l.add(new Pair<>(timestamp[i], website[i]));
+      H.put(username[i], l);
+    }
+
+    HashMap<String, Integer> fq = new HashMap<>();
+
+    String result = null;
+
+    for (String u : H.keySet()) {
+      HashSet<String> set = new HashSet<>();
+      ArrayList<Pair<Integer, String>> l = H.get(u);
+      l.sort((Pair<Integer, String> pair1, Pair<Integer, String> pair2) -> {
+        return pair1.getKey() - pair2.getKey();
+      });
+
+      for (int i = 0; i < l.size()-2; i++) {
+        for (int j = i+1; j < l.size()-1; j++) {
+          for (int s = j+1; s < l.size(); s++) {
+            Pair<Integer, String> pi = l.get(i);
+            Pair<Integer, String> pj = l.get(j);
+            Pair<Integer, String> ps = l.get(s);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(pi.getValue());
+            sb.append(" ");
+            sb.append(pj.getValue());
+            sb.append(" ");
+            sb.append(ps.getValue());
+            String key = sb.toString();
+            if (!set.contains(key)) {
+              fq.put(key, fq.getOrDefault(key, 0) + 1);
+              set.add(key);
+            }
+
+            if (result == null || fq.get(result) < fq.get(key) || (fq.get(result) == fq.get(key) && result.compareTo(key) > 0)) {
+              result = key;
+            }
+          }
+        }
+      }
+    }
+
+    String[] resArr = result.split(" ");
+    List<String> output = new ArrayList<>();
+    for (String p : resArr) {
+      output.add(p);
+    }
+    return output;
+  }
 }
