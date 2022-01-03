a2faf8a9710530dc90b8fec6dad479e24a2cf909
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-01-02 18:50:12 -0800

    642 design search autocomplete system

--- algo/leetcode/642-design-search-autocomplete-system/src/com/company/AutocompleteSystem.java
+++ algo/leetcode/642-design-search-autocomplete-system/src/com/company/AutocompleteSystem.java
@@ -1,24 +1,42 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import javafx.util.Pair;
+import java.util.stream.Collectors;
 
 public class AutocompleteSystem {
-  class Node {
-    Node[] items = new Node[27];
-    boolean isEOS = false;
+  class Node implements Comparable<Node> {
+    Node[] children = new Node[27];
+    List<Node> hot = new ArrayList<>();
     int w = 0;
-    char c;
+    String s = "";
+
+    @Override
+    public int compareTo(Node o) {
+      return this.w == o.w ? this.s.compareTo(o.s) : o.w - this.w;
+    }
+
+    public void update(Node n) {
+      if (!hot.contains(n)) {
+        hot.add(n);
+      }
+
+      Collections.sort(hot);
+      if (hot.size() > 3) {
+        hot.remove(hot.size()-1);
+      }
+    }
   }
 
   private Node root = new Node();
 
   private Node currNode = root;
-  private String currSentence = null;
+  private String currSentence = "";
+
+  private int idx(char c) {
+    return c == ' ' ? 26 : c - 'a';
+  }
 
   public AutocompleteSystem(String[] sentences, int[] times) {
     for (int i = 0; i < sentences.length; i++) {
@@ -26,137 +44,54 @@ public class AutocompleteSystem {
     }
   }
 
-  public List<String> input(char c) {
-    List<String> output = new ArrayList<>();
-
-    if (c != '#' && currNode != null && currNode.items[idx(c)] != null) {
-      if (currSentence == null) {
-        currSentence = "";
-      }
-      currSentence += c;
-      currNode = currNode.items[idx(c)];
-      output = top(3);
-    } else {
-      if (c == '#') {
-        currSentence += c;
-        output = top(3);
-        add(currSentence);
-
-
-
-      } else {
-        currSentence += c;
-        currNode = null;
-      }
-
-      if (currSentence != "" ) {
-        if (currNode != null) {
-          add(currSentence, currNode.w + 1);
-        } else {
-          add(currSentence, 1);
-        }
-      }
-
-      // add to trie, if not exists with w 1, mark currNode as isEOS = true
-      // increment weight by 1 if exists
-
-      if (currSentence != null) {
-        output.add(currSentence);
-      }
-      currNode = root;
-      currSentence = null;
-      return output;
-
-
-
-    }
-
-    List<String> output = new ArrayList<>();
-    currNode = root;
-    currSentence = "";
-    return output;
-  }
-
   private void add(String sentence, int w) {
     if (sentence.length() == 0) {
       return;
     }
-    sentence += "#";
-
     Node n = root;
+    List<Node> toUpdate = new ArrayList<>();
+    toUpdate.add(root);
+
+    String sent = "";
     for (int i = 0; i < sentence.length(); i++) {
       char c = sentence.charAt(i);
-      if (c == '#') {
-        n.isEOS = true;
-        break;
-      }
-
+      sent += c;
       int idx = idx(c);
-      Node next = n.items[idx];
-      if (next != null) {
-        next.w = Math.max(next.w, w);
-      } else {
+      Node next = n.children[idx];
+      if (next == null) {
         next = new Node();
-        next.c = c;
-        next.w = w;
+        next.s = sent;
       }
-
-      n.items[idx] = next;
+      n.children[idx] = next;
       n = next;
+      toUpdate.add(n);
+    }
+
+    n.w = n.w + w;
+
+    for (Node node : toUpdate) {
+      node.update(n);
     }
   }
 
-  private int idx(char c) {
-    return c == ' ' ? 26 : c - 'a';
-  }
-
-  private List<String> top(int topN) {
-    PriorityQueue<Pair<Node, String>> outputQ = new PriorityQueue<Pair<Node, String>>((p1, p2) -> {
-      return p1.getKey().w == p2.getKey().w
-          ? p1.getValue().compareTo(p2.getValue())
-          : p2.getKey().w - p1.getKey().w;
-    });
-
-    if (currNode == null) {
-      return new ArrayList<>();
-    }
-    Queue<Node> nds = new LinkedList<>();
-    Queue<String> sents = new LinkedList<>();
-    nds.add(currNode);
-    sents.add(currSentence);
-    while(!nds.isEmpty()) {
-      Node nd = nds.poll();
-      String sent = sents.poll();
-
-      if (nd.isEOS) {
-        outputQ.add(new Pair<>(nd, sent));
-      }
-
-      PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> {
-        return n1.w == n2.w ? Character.compare(n2.c, n1.c) : n2.w - n1.w;
-      });
-
-      for (int i = 0; i < nd.items.length; i++) {
-        if (nd.items[i] != null) {
-          pq.add(nd.items[i]);
-        }
-      }
-
-      int counter = 0;
-      while (counter < 3 && pq.size() > 0) {
-        Node nxt = pq.poll();
-        nds.add(nxt);
-        sents.add(sent + nxt.c);
-      }
-    }
-
+  public List<String> input(char c) {
     List<String> output = new ArrayList<>();
-    while (output.size() < topN && outputQ.size() > 0) {
-      Pair<Node, String> p = outputQ.poll();
-      if (!p.getValue().isEmpty()) {
-        output.add(p.getValue());
-      }
+    if (c == '#') {
+      add(currSentence, 1);
+      currNode = root;
+      currSentence = "";
+      return output;
     }
+
+    currSentence += c;
+    if (currNode != null) {
+      currNode = currNode.children[idx(c)];
+    }
+
+    if (currNode != null) {
+      output = currNode.hot.stream().map((Node n) -> n.s).collect(Collectors.toList());
+    }
+
     return output;
   }
 
--- algo/leetcode/642-design-search-autocomplete-system/src/com/company/Main.java
+++ algo/leetcode/642-design-search-autocomplete-system/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -9,15 +10,130 @@ public class Main {
         new String[]{"i love you", "island", "iroman", "i love leetcode"},
         new int[]{5, 3, 2, 2});
 
+    boolean passed = true;
     List<String> output1 = as.input('i');
+    passed = passed && Arrays.deepEquals(output1.toArray(), new String[]{"i love you", "island", "i love leetcode"});
+
     List<String> output2 = as.input(' ');
+    passed = passed && Arrays.deepEquals(output2.toArray(), new String[]{"i love you", "i love leetcode"});
+
     List<String> output3 = as.input('a');
+    passed = passed && Arrays.deepEquals(output3.toArray(), new String[]{});
+
     List<String> output4 = as.input('#');
+    passed = passed && Arrays.deepEquals(output4.toArray(), new String[]{});
+
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    AutocompleteSystem as = new AutocompleteSystem(
+        new String[]{"i love you","island","iroman","i love leetcode"},
+        new int[]{5, 3, 2, 2});
+
+    boolean passed = true;
 
 
+    List<String> output1 = as.input('i');
+    passed = passed && Arrays.deepEquals(output1.toArray(), new String[]{"i love you", "island", "i love leetcode"});
+
+    List<String> output2 = as.input(' ');
+    passed = passed && Arrays.deepEquals(output2.toArray(), new String[]{"i love you", "i love leetcode"});
+
+    List<String> output3 = as.input('a');
+    passed = passed && Arrays.deepEquals(output3.toArray(), new String[]{});
+
+    List<String> output4 = as.input('#');
+    passed = passed && Arrays.deepEquals(output4.toArray(), new String[]{});
+
+
+    List<String> output5 = as.input('i');
+    passed = passed && Arrays.deepEquals(output5.toArray(), new String[]{"i love you", "island", "i love leetcode"});
+
+    List<String> output6 = as.input(' ');
+    passed = passed && Arrays.deepEquals(output6.toArray(), new String[]{"i love you", "i love leetcode", "i a"});
+
+    List<String> output7 = as.input('a');
+    passed = passed && Arrays.deepEquals(output7.toArray(), new String[]{"i a"});
+
+    List<String> output8 = as.input('#');
+    passed = passed && Arrays.deepEquals(output8.toArray(), new String[]{});
+
+
+    List<String> output9 = as.input('i');
+    passed = passed && Arrays.deepEquals(output9.toArray(), new String[]{"i love you","island","i a"});
+
+    List<String> output10 = as.input(' ');
+    passed = passed && Arrays.deepEquals(output10.toArray(), new String[]{"i love you","i a","i love leetcode"});
+
+    List<String> output11 = as.input('a');
+    passed = passed && Arrays.deepEquals(output11.toArray(), new String[]{"i a"});
+
+    List<String> output12 = as.input('#');
+    passed = passed && Arrays.deepEquals(output12.toArray(), new String[]{});
+
+
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test3() {
+    AutocompleteSystem as = new AutocompleteSystem(
+        new String[]{"abc","abbc","a"},
+        new int[]{3,3,3});
+
+    boolean passed = true;
+
+    List<String> output1 = as.input('b');
+    passed = passed && Arrays.deepEquals(output1.toArray(), new String[]{});
+
+    List<String> output2 = as.input('c');
+    passed = passed && Arrays.deepEquals(output2.toArray(), new String[]{});
+
+    List<String> output3 = as.input('#');
+    passed = passed && Arrays.deepEquals(output3.toArray(), new String[]{});
+
+
+    List<String> output4 = as.input('b');
+    passed = passed && Arrays.deepEquals(output4.toArray(), new String[]{"bc"});
+
+    List<String> output5 = as.input('c');
+    passed = passed && Arrays.deepEquals(output5.toArray(), new String[]{"bc"});
+
+    List<String> output6 = as.input('#');
+    passed = passed && Arrays.deepEquals(output6.toArray(), new String[]{});
+
+
+    List<String> output7 = as.input('a');
+    passed = passed && Arrays.deepEquals(output7.toArray(), new String[]{"a","abbc","abc"});
+
+    List<String> output8 = as.input('b');
+    passed = passed && Arrays.deepEquals(output8.toArray(), new String[]{"abbc","abc"});
+
+    List<String> output9 = as.input('c');
+    passed = passed && Arrays.deepEquals(output9.toArray(), new String[]{"abc"});
+
+    List<String> output10 = as.input('#');
+    passed = passed && Arrays.deepEquals(output10.toArray(), new String[]{});
+
+
+    List<String> output11 = as.input('a');
+    passed = passed && Arrays.deepEquals(output11.toArray(), new String[]{"abc","a","abbc",});
+
+    List<String> output12 = as.input('b');
+    passed = passed && Arrays.deepEquals(output12.toArray(), new String[]{"abc","abbc"});
+
+    List<String> output13 = as.input('c');
+    passed = passed && Arrays.deepEquals(output13.toArray(), new String[]{"abc"});
+
+    List<String> output14 = as.input('#');
+    passed = passed && Arrays.deepEquals(output14.toArray(), new String[]{});
+
+    System.out.println("test3: " + (passed ? "passed" : "failed"));
   }
 
   public static void main(String[] args) {
     test1();
+    test2();
+    test3();
   }
 }
