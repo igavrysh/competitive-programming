1eed9c01ff502c083b8819640cf86d151900347e
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-01-10 03:16:14 -0800

    solution for problem 1463 cherry pickup ii

--- algo/leetcode/1463-cherry-pickup-ii/src/com/company/Main.java
+++ algo/leetcode/1463-cherry-pickup-ii/src/com/company/Main.java
@@ -2,7 +2,33 @@ package com.company;
 
 public class Main {
 
+  public static void test1() {
+    int[][] grid = {{3,1,1},{2,5,1},{1,5,5},{2,1,1}};
+    Solution s = new Solution();
+    int output = s.cherryPickup(grid);
+    boolean passed = output == 24;
+    System.out.println("test1:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    int[][] grid = {{1,0,0,0,0,0,1},{2,0,0,0,0,3,0},{2,0,9,0,0,0,0},{0,3,0,5,4,0,0},{1,0,2,3,0,0,6}};
+    Solution s = new Solution();
+    int output = s.cherryPickup(grid);
+    boolean passed = output == 28;
+    System.out.println("test2:" + (passed ? "passed" : "failed"));
+  }
+
+  public static void test3() {
+    int[][] grid = {{0,8,7,10,9,10,0,9,6},{8,7,10,8,7,4,9,6,10},{8,1,1,5,1,5,5,1,2},{9,4,10,8,8,1,9,5,0},{4,3,6,10,9,2,4,8,10},{7,3,2,8,3,3,5,9,8},{1,2,6,5,6,2,0,10,0}};
+    Solution s = new Solution();
+    int output = s.cherryPickup(grid);
+    boolean passed = output == 96;
+    System.out.println("test3:" + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
+    test3();
   }
 }
--- algo/leetcode/1463-cherry-pickup-ii/src/com/company/Solution.java
+++ algo/leetcode/1463-cherry-pickup-ii/src/com/company/Solution.java
@@ -4,8 +4,54 @@ public class Solution {
   public int cherryPickup(int[][] grid) {
     int rows = grid.length;
     int cols = grid[0].length;
-    int[][][] DP = new int[rows][cols][2];
+    int[][][] DP = new int[rows][cols][cols];
+    for (int j1 = 0; j1 < cols; j1++) {
+      for (int j2 = 0; j2 < cols; j2++) {
+        if (j1 == 0 && j2 == cols-1) {
+          DP[0][j1][j2] = grid[0][0] + grid[0][cols-1];
+        } else {
+          DP[0][j1][j2] = -1;
+        }
+      }
+    }
 
-    return -1;
+    for (int i = 1; i < rows; i++) {
+      for (int j1 = 0; j1 < cols; j1++) {
+        for (int j2 = 0; j2 < cols; j2++) {
+          int maxDP = -1;
+          for (int d1 = -1; d1 <= 1; d1++) {
+            for (int d2 = -1; d2 <= 1; d2++) {
+              if (j1+d1 >= 0 && j1+d1<cols && j2+d2>=0 && j2+d2<cols) {
+                if (maxDP == -1 || maxDP < DP[i-1][j1+d1][j2+d2]) {
+                  maxDP = DP[i-1][j1+d1][j2+d2];
+                }
+              }
+            }
+          }
+          int delta = 0;
+          if (j1 == j2) {
+            delta = grid[i][j1];
+          } else {
+            delta = grid[i][j1] + grid[i][j2];
+          }
+
+          if (maxDP == -1) {
+            DP[i][j1][j2] = -1;
+          } else {
+            DP[i][j1][j2] = maxDP + delta;
+          }
+        }
+      }
+    }
+
+    int result = -1;
+    for (int j1 = 0; j1 < cols; j1++) {
+      for (int j2 = 0; j2 < cols; j2++) {
+        if (result == -1 || result < DP[rows-1][j1][j2]) {
+          result = DP[rows-1][j1][j2];
+        }
+      }
+    }
+    return result;
   }
 }
