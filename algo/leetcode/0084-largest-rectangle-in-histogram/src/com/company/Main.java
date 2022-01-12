c29dbec43fbc3c7cac3384468c460878dbea5647
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-01-11 19:28:15 -0800

    solution for problem 84 largest retangle in histogram

--- algo/leetcode/0084-largest-rectangle-in-histogram/src/com/company/Main.java
+++ algo/leetcode/0084-largest-rectangle-in-histogram/src/com/company/Main.java
@@ -2,7 +2,33 @@ package com.company;
 
 public class Main {
 
+  public static void test1() {
+    int[] heights = {2,1,5,6,2,3};
+    SolutionStack s = new SolutionStack();
+    int output = s.largestRectangleArea(heights);
+    boolean passed = output == 10;
+    System.out.println("test1: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test2() {
+    int[] heights = {2,4};
+    SolutionStack s = new SolutionStack();
+    int output = s.largestRectangleArea(heights);
+    boolean passed = output == 4;
+    System.out.println("test2: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void test3() {
+    int[] heights = {2,1,2};
+    SolutionStack s = new SolutionStack();
+    int output = s.largestRectangleArea(heights);
+    boolean passed = output == 3;
+    System.out.println("test3: " + (passed ? "passed" : "failed"));
+  }
+
   public static void main(String[] args) {
-    // write your code here
+    test1();
+    test2();
+    test3();
   }
 }
---
+++ algo/leetcode/0084-largest-rectangle-in-histogram/src/com/company/SolutionStack.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Stack;
+
+public class SolutionStack {
+  public int largestRectangleArea(int[] heights) {
+    Stack<Integer> S = new Stack<>();
+    int maxSize = 0;
+    int zeroIndex = -1;
+    // Last step, i = heights.length is required to process stack of nondecreasing height indexes
+    for (int i = 0; i <= heights.length; i++) {
+
+      while (S.size() != 0 && (i == heights.length || heights[S.peek()] > heights[i])) {
+        int idx = S.pop();
+        int nextSize = 0;
+        if (S.size() != 0) {
+          int prevIndex = S.peek();
+          nextSize = (i - prevIndex - 1) * heights[idx];
+        } else {
+          nextSize = (i-1 - zeroIndex) * heights[idx];
+        }
+        if (maxSize < nextSize) {
+          maxSize = nextSize;
+        }
+      }
+
+      S.push(i);
+
+      if (i < heights.length && heights[i] == 0) {
+        zeroIndex = i;
+      }
+    }
+    return maxSize;
+  }
+
+}
