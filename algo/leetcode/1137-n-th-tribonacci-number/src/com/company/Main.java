842d894b72dfb4bea8628e8e0526541712da9842
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-01-14 14:41:47 -0800

    solution for 1137 n th tribonacci number problem

--- algo/leetcode/1137-n-th-tribonacci-number/src/com/company/Main.java
+++ algo/leetcode/1137-n-th-tribonacci-number/src/com/company/Main.java
@@ -2,7 +2,23 @@ package com.company;
 
 public class Main {
 
+    public static void test1() {
+        Solution s = new Solution();
+        int output = s.tribonacci(4);
+        boolean passed = output == 4;
+        System.out.println("test1: " + (passed ? "passed" : "failed"));
+    }
+
+    public static void test2() {
+        Solution s = new Solution();
+        int output = s.tribonacci(25);
+        boolean passed = output == 1389537;
+        System.out.println("test2: " + (passed ? "passed" : "failed"));
+    }
+
+
     public static void main(String[] args) {
-	// write your code here
+	      test1();
+	      test2();
     }
 }
