09ccd86f805a5689527eb92a158d0bcd6b466a3d
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-01-17 12:25:44 -0800

    add one HashMap solution for problem 290 word pattern

--- algo/leetcode/0290-word-pattern/src/com/company/Main.java
+++ algo/leetcode/0290-word-pattern/src/com/company/Main.java
@@ -2,42 +2,52 @@ package com.company;
 
 public class Main {
 
-  public static void test1() {
+  public static void testSolutionTwoHashMaps1() {
     String pattern = "abba", s = "dog cat cat dog";
     SolutionTwoHashMaps solutionTwoHashMaps = new SolutionTwoHashMaps();
     boolean output = solutionTwoHashMaps.wordPattern(pattern, s);
     boolean passed = output == true;
-    System.out.println("test1: " + (passed ? "passed" : "failed"));
+    System.out.println("testSolutionTwoHashMaps1: " + (passed ? "passed" : "failed"));
   }
 
-  public static void test2() {
+  public static void testSolutionTwoHashMaps2() {
     String pattern = "abba", s = "dog cat cat fish";
     SolutionTwoHashMaps solutionTwoHashMaps = new SolutionTwoHashMaps();
     boolean output = solutionTwoHashMaps.wordPattern(pattern, s);
     boolean passed = output == false;
-    System.out.println("test2: " + (passed ? "passed" : "failed"));
+    System.out.println("testSolutionTwoHashMaps2: " + (passed ? "passed" : "failed"));
   }
 
-  public static void test3() {
+  public static void testSolutionTwoHashMaps3() {
     String pattern = "aaaa", s = "dog cat cat dog";
     SolutionTwoHashMaps solutionTwoHashMaps = new SolutionTwoHashMaps();
     boolean output = solutionTwoHashMaps.wordPattern(pattern, s);
     boolean passed = output == false;
-    System.out.println("test3: " + (passed ? "passed" : "failed"));
+    System.out.println("testSolutionTwoHashMaps3: " + (passed ? "passed" : "failed"));
   }
 
-  public static void test4() {
+  public static void testSolutionTwoHashMaps4() {
     String pattern = "abba", s = "dog dog dog dog";
     SolutionTwoHashMaps solutionTwoHashMaps = new SolutionTwoHashMaps();
     boolean output = solutionTwoHashMaps.wordPattern(pattern, s);
     boolean passed = output == false;
-    System.out.println("test4: " + (passed ? "passed" : "failed"));
+    System.out.println("testSolutionTwoHashMaps4: " + (passed ? "passed" : "failed"));
+  }
+
+  public static void testSolutionOneHashMap1() {
+    String pattern = "abba", s = "dog a a dog";
+    SolutionTwoHashMaps solutionTwoHashMaps = new SolutionTwoHashMaps();
+    boolean output = solutionTwoHashMaps.wordPattern(pattern, s);
+    boolean passed = output == true;
+    System.out.println("testSolutionOneHashMap1: " + (passed ? "passed" : "failed"));
   }
 
   public static void main(String[] args) {
-    test1();
-    test2();
-    test3();
-    test4();
+    testSolutionTwoHashMaps1();
+    testSolutionTwoHashMaps2();
+    testSolutionTwoHashMaps3();
+    testSolutionTwoHashMaps4();
+
+    testSolutionOneHashMap1();
   }
 }
