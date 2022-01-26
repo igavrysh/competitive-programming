9ae87421a557d47eb289fbd3c327401639db367e
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-01-25 17:15:25 -0800

    improved solution for problem 85 maximal rectangle

--- algo/leetcode/0085-maximal-rectangle/src/com/company/SolutionDP.java
+++ algo/leetcode/0085-maximal-rectangle/src/com/company/SolutionDP.java
@@ -7,61 +7,51 @@ public class SolutionDP {
     int N = matrix.length;
     int M = matrix[0].length;
     int[] h = new int[M], l = new int[M], r = new int[M];
+    Arrays.fill(r, M);
+    Arrays.fill(l, -1);
     int res = 0;
     for (int i = 0; i < N; i++) {
       int currLeft = -1;
-      int currRight = Integer.MAX_VALUE;
+      int currRight = M;
       int[] hloc = new int[M], lloc = new int[M], rloc = new int[M];
-      Arrays.fill(rloc, Integer.MAX_VALUE);
 
       for (int j = 0; j < M; j++) {
         if (matrix[i][j] == '1') {
           hloc[j] = h[j] + 1;
+        } else {
+          hloc[j] = 0;
+        }
+      }
+
+      for (int j = 0; j < M; j++) {
+        if (matrix[i][j] == '1') {
           if (currLeft == -1) {
             currLeft = j;
           }
         } else {
-          hloc[j] = 0;
+          l[j] = -1;
           currLeft = -1;
         }
+        lloc[j] = Math.max(currLeft, l[j]);
+      }
 
-        if (i==0) {
-          lloc[j] = currLeft;
-        } else {
-          if (matrix[i][j] == '0') {
-            lloc[j] = -1;
-          } else {
-            lloc[j] = Math.max(currLeft, l[j]);
-          }
-        }
-
-        if (matrix[i][M-1-j] == '1') {
-          hloc[M-1-j] = h[M-1-j] + 1;
-          if (currRight == Integer.MAX_VALUE) {
-            currRight = M-1-j;
+      for (int j = M-1; j >= 0; j--) {
+        if (matrix[i][j] == '1') {
+          if (currRight == M) {
+            currRight = j;
           }
         } else {
-          hloc[M-1-j] = 0;
-          currRight = Integer.MAX_VALUE;
+          r[j] = M;
+          currRight = M;
         }
-
-        if (i==0) {
-          rloc[M-1-j] = currRight;
-        } else {
-          if (matrix[i][M-1-j] == '0') {
-            rloc[M-1-j] = Integer.MAX_VALUE;
-          } else {
-            rloc[M - 1 - j] = Math.min(currRight, r[M - 1 - j]);
-          }
-        }
+        rloc[j] = Math.min(currRight, r[j]);
       }
 
       h = hloc;
       r = rloc;
       l = lloc;
-      int t = 1;
       for (int j = 0; j < M; j++) {
-        if (r[j] != Integer.MAX_VALUE && l[j] != -1 && h[j] > 0) {
+        if (r[j] != M && l[j] != -1 && h[j] > 0) {
           int area = h[j] * (r[j]-l[j]+1);
           if (res < area) {
             res = area;
