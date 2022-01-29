d664a9afee7e3dfc13e6340f1cf1f92164de88de
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-01-29 11:21:47 -0800

    solution for problem  84

---
+++ algo/leetcode/0084-largest-rectangle-in-histogram/src/com/company/Solution2.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Solution2 {
+  public int largestRectangleArea(int[] heights) {
+    Stack<Integer> S = new Stack<>();
+    S.push(-1);
+    int res = 0;
+    for (int i = 0; i <= heights.length; i++) {
+      while (S.peek() != -1 && (i == heights.length || heights[S.peek()] > heights[i])) {
+        int idx = S.pop();
+        int area = (i-1 - S.peek()) * heights[idx];
+        if (area > res) {
+          res = area;
+        }
+      }
+      if (i != heights.length) {
+        S.push(i);
+      }
+    }
+    return res;
+  }
+}
