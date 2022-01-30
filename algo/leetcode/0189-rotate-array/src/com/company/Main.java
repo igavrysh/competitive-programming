6392883f30b7ff0ce9dfa2ef15c864531904723b
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-01-30 10:45:12 -0800

    try alternative solution for problem 189 rotate array

--- algo/leetcode/0189-rotate-array/src/com/company/Main.java
+++ algo/leetcode/0189-rotate-array/src/com/company/Main.java
@@ -4,16 +4,46 @@ import java.util.Arrays;
 
 class Main {
   
-  public static void test1() {
+  public static void testSolution1() {
     int[] nums = {1,2,3,4,5,6,7};
     int k = 3;
     Solution s = new Solution();
     s.rotate(nums, k);
     boolean passed = Arrays.equals(nums, new int[]{5,6,7,1,2,3,4});
-    System.out.println("test1: " + (passed ? "passed" : "failed") );
+    System.out.println("testSolution1: " + (passed ? "passed" : "failed") );
+  }
+
+  public static void testSolutionFaster1() {
+    int[] nums = {1,2,3,4,5,6,7};
+    int k = 3;
+    SolutionFaster s = new SolutionFaster();
+    s.rotate(nums, k);
+    boolean passed = Arrays.equals(nums, new int[]{5,6,7,1,2,3,4});
+    System.out.println("testSolutionFaster1: " + (passed ? "passed" : "failed") );
+  }
+
+  public static void testSolutionFaster2() {
+    int[] nums = {1,2,3,4,5,6};
+    int k = 3;
+    SolutionFaster s = new SolutionFaster();
+    s.rotate(nums, k);
+    boolean passed = Arrays.equals(nums, new int[]{4,5,6,1,2,3});
+    System.out.println("testSolutionFaster2: " + (passed ? "passed" : "failed") );
+  }
+
+  public static void testSolutionFaster3() {
+    int[] nums = {-1,-100,3,99};
+    int k = 3;
+    SolutionFaster s = new SolutionFaster();
+    s.rotate(nums, k);
+    boolean passed = Arrays.equals(nums, new int[]{-100,3,99,01});
+    System.out.println("testSolutionFaster3: " + (passed ? "passed" : "failed") );
   }
 
   public static void main(String[] args) {
-    test1();
+    testSolution1();
+    testSolutionFaster1();
+    testSolutionFaster2();
+    testSolutionFaster3();
   }
 }
---
+++ algo/leetcode/0189-rotate-array/src/com/company/SolutionFaster.java
@@ -0,0 +1,28 @@
+package com.company;
+
+class SolutionFaster {
+  public void rotate(int[] nums, int k) {
+    k=k%nums.length;
+    cycle(0, nums, k);
+    if (nums.length%2==0) {
+      for (int i = 1; i < k; i++) {
+        cycle(i, nums, k);
+      }
+    }
+  }
+
+  private void cycle(int s, int[] nums, int k) {
+    int nextIdx = (s+k)%nums.length;
+    int tmp = nums[s];
+    int i = s;
+    boolean firstPassed = false;
+    while (!(i==s && firstPassed==true)) {
+      firstPassed = true;
+      nextIdx = (i+k)%nums.length;
+      int tmp2  = nums[nextIdx];
+      nums[nextIdx] = tmp; 
+      tmp = tmp2;
+      i=nextIdx;
+    }
+  }
+}
