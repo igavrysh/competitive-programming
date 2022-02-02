17a8710bf8ad632656baf73863c7f4471854dfb1
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2022-02-02 11:50:16 -0800

    small optimization for 438 problem

--- algo/leetcode/0438-find-all-anagrams-in-a-string/src/com/company/Solution.java
+++ algo/leetcode/0438-find-all-anagrams-in-a-string/src/com/company/Solution.java
@@ -5,30 +5,33 @@ import java.util.ArrayList;
 
 class Solution {
   public List<Integer> findAnagrams(String s, String p) {
-    int[] fq = new int[26];
+    int ns = s.length(), np = p.length();
+    if (np > ns) {
+      return new ArrayList<>();
+    }
+
+    int[] fqP = new int[26];
     for (char c : p.toCharArray()) {
-      fq[c-'a']++;
+      fqP[c-'a']++;
     }
     List<Integer> res = new ArrayList<>();
-    int[] actualFq = new int[26];
+    int[] fqS = new int[26];
     int matched = 0;
-    for (int i = 0; i < s.length(); i++) {
+    for (int i = 0; i < ns; i++) {
       if (i >= p.length()) {
-        char toRemove = s.charAt(i-p.length());
-        actualFq[toRemove-'a']--;
-        if (fq[toRemove-'a'] > 0 && actualFq[toRemove-'a'] < fq[toRemove-'a']) {
+        char toRemove = s.charAt(i-np);
+        fqS[toRemove-'a']--;
+        if (fqP[toRemove-'a'] > 0 && fqS[toRemove-'a'] < fqP[toRemove-'a']) {
           matched--;
         }
       }
       char c = s.charAt(i);
-      actualFq[c-'a']++;
-      if (fq[c-'a'] > 0 && actualFq[c-'a'] <= fq[c-'a']) {
+      fqS[c-'a']++;
+      if (fqP[c-'a'] > 0 && fqS[c-'a'] <= fqP[c-'a']) {
         matched++;
       }
-      if (i >= p.length()-1) {
-        if (matched == p.length()) {
-          res.add(i-(p.length()-1));
-        }
+      if (i >= p.length()-1 && matched == p.length()) {
+        res.add(i-(p.length()-1));
       }
     }
     return res;
