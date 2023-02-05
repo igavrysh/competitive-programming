41e281613bcdbb4517961269b3bdccfd94b6440a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-02-04 16:37:53 -0800

    monotonic stack solution for problem 739 daily temperatures

--- algo/leetcode/0739-daily-temperatures/src/com/company/Main.java
+++ algo/leetcode/0739-daily-temperatures/src/com/company/Main.java
@@ -7,6 +7,10 @@ class Main {
         testSolutionSimple1();
         testSolutionSimple2();
         testSolutionSimple3();
+
+        testSolutionMonotonicStack1();
+        testSolutionMonotonicStack2();
+        testSolutionMonotonicStack3();
     }
 
     public static void testSolutionSimple1() {
@@ -32,4 +36,29 @@ class Main {
         boolean passed = Arrays.equals(output, new int[]{1,1,0});
         System.out.println("testSolutionSimple3: " + (passed ? "passed" : "failed"));
     }
+
+    public static void testSolutionMonotonicStack1() {
+        int[] temperatures = new int[]{73,74,75,71,69,72,76,73};
+        SolutionMonotonicStack s = new SolutionMonotonicStack();
+        int[] output = s.dailyTemperatures(temperatures);
+        boolean passed = Arrays.equals(output, new int[]{1,1,4,2,1,1,0,0});
+        System.out.println("testSolutionMonotonicStack1: " + (passed ? "passed" : "failed"));
+    }
+
+    public static void testSolutionMonotonicStack2() {
+        int[] temperatures = new int[]{30,40,50,60};
+        SolutionMonotonicStack s = new SolutionMonotonicStack();
+        int[] output = s.dailyTemperatures(temperatures);
+        boolean passed = Arrays.equals(output, new int[]{1,1,1,0});
+        System.out.println("testSolutionMonotonicStack2: " + (passed ? "passed" : "failed"));
+    }
+
+    public static void testSolutionMonotonicStack3() {
+        int[] temperatures = new int[]{30,60,90};
+        SolutionMonotonicStack s = new SolutionMonotonicStack();
+        int[] output = s.dailyTemperatures(temperatures);
+        boolean passed = Arrays.equals(output, new int[]{1,1,0});
+        System.out.println("testSolutionMonotonicStack3: " + (passed ? "passed" : "failed"));
+    }
+
 }
--- algo/leetcode/0739-daily-temperatures/src/com/company/SolutionMonotonicStack.java
+++ algo/leetcode/0739-daily-temperatures/src/com/company/SolutionMonotonicStack.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.Stack;
+
 class SolutionMonotonicStack {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] output = new int[temperatures.length];
-        
-        return null;
+        Stack<Integer> S = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (S.size() > 0 && temperatures[S.peek()] < temperatures[i]) {
+                int idx = S.pop();
+                output[idx] = i-idx;
+            }
+            S.push(i);
+        }
+        return output;
     }
-
 }
