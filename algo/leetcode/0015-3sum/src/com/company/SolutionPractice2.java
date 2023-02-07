171290cb434c8dc2b1098b2685c0bc865854423f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-02-07 14:06:23 -0800

    finalized solution for problem 15 3sum practice 2

--- algo/leetcode/0015-3sum/src/com/company/SolutionPractice2.java
+++ algo/leetcode/0015-3sum/src/com/company/SolutionPractice2.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+// edge cases: [0, 0, 0, 0], [-2, 0, 1, 1, 2]
 public class SolutionPractice2 {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> output = new ArrayList<>();
         for (int i = 0; i < nums.length-2; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                i++;
-                continue;
-            }
             int t = -nums[i];
             int l = i+1;
             int r = nums.length-1;
@@ -22,19 +19,21 @@ public class SolutionPractice2 {
                     output.add(Arrays.asList(nums[i], nums[l], nums[r]));
                 }
 
-                while (l+1<nums.length-1 && nums[l]==nums[l+1]) {
+                if (l<nums.length && r>i && nums[l]+nums[r]<=t) {
+                    while (l+1 < nums.length && nums[l] == nums[l+1] && l+1<r) {
+                        l++;
+                    }
                     l++;
+                } else if (l<nums.length && r>i && nums[l]+nums[r]>=t) {
+                    while (r-1 >= i && nums[r] == nums[r-1] && l<r-1) {
+                        r--;
+                    }
+                    r--;
                 }
+            }
 
-                while (r-1>=0 && nums[r]==nums[r-1]) {
-                    r--;
-                }
-                
-                if (nums[l]+nums[r]<=t) {
-                    l++;
-                } else if (nums[l]+nums[r]>t) {
-                    r--;
-                }
+            while (i >= 0 && i+1 <nums.length && nums[i] == nums[i+1]) {
+                i++;
             }
         }
         return output;
