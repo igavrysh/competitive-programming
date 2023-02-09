1155e9d27525934390f95428849147a17cc4ac9f
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-02-08 20:20:21 -0800

    small fixes for problem 11

--- algo/leetcode/0011-container-with-most-water/src/com/company/Main.java
+++ algo/leetcode/0011-container-with-most-water/src/com/company/Main.java
@@ -2,92 +2,93 @@ package com.company;
 
 public class Main {
 
-  private static void test1() {
-    int[] height = { 1, 1};
-    SolutionBruteForce s = new SolutionBruteForce();
-    int output = s.maxArea(height);
-    boolean passed = output == 1;
-    System.out.println("test1:" + (passed ? "passed" : "failed"));
-  }
+    public static void main(String[] args) {
 
-  private static void test2() {
-    int[] height = { 4, 3, 2, 1, 4 };
-    SolutionBruteForce s = new SolutionBruteForce();
-    int output = s.maxArea(height);
-    boolean passed = output == 16;
-    System.out.println("test2:" + (passed ? "passed" : "failed"));
-  }
+        test1();
+        test2();
+        test3();
+        test4();
+        test5();
+        test6();
 
-  private static void test3() {
-    int[] height = { 1, 2, 1 };
-    SolutionBruteForce s = new SolutionBruteForce();
-    int output = s.maxArea(height);
-    boolean passed = output == 2;
-    System.out.println("test3:" + (passed ? "passed" : "failed"));
-  }
+        test7();
 
-  private static void test4() {
-    int[] height = { 1, 1};
-    SolutionTwoPointers s = new SolutionTwoPointers();
-    int output = s.maxArea(height);
-    boolean passed = output == 1;
-    System.out.println("test4:" + (passed ? "passed" : "failed"));
-  }
+        test8();
+        test9();
+    }
 
-  private static void test5() {
-    int[] height = { 4, 3, 2, 1, 4 };
-    SolutionTwoPointers s = new SolutionTwoPointers();
-    int output = s.maxArea(height);
-    boolean passed = output == 16;
-    System.out.println("test5:" + (passed ? "passed" : "failed"));
-  }
+    private static void test1() {
+        int[] height = { 1, 1 };
+        SolutionBruteForce s = new SolutionBruteForce();
+        int output = s.maxArea(height);
+        boolean passed = output == 1;
+        System.out.println("test1:" + (passed ? "passed" : "failed"));
+    }
 
-  private static void test6() {
-    int[] height = { 1, 2, 1 };
-    SolutionTwoPointers s = new SolutionTwoPointers();
-    int output = s.maxArea(height);
-    boolean passed = output == 2;
-    System.out.println("test6:" + (passed ? "passed" : "failed"));
-  }
+    private static void test2() {
+        int[] height = { 4, 3, 2, 1, 4 };
+        SolutionBruteForce s = new SolutionBruteForce();
+        int output = s.maxArea(height);
+        boolean passed = output == 16;
+        System.out.println("test2:" + (passed ? "passed" : "failed"));
+    }
 
-  private static void test7() {
-    int [] height = {1,8,6,2,5,4,8,3,7};
-    SolutionTwoPointers s = new SolutionTwoPointers();
-    int output = s.maxArea(height);
-    boolean passed = output == 49;
-    System.out.println("test7:" + (passed ? "passed" : "failed"));
-  }
+    private static void test3() {
+        int[] height = { 1, 2, 1 };
+        SolutionBruteForce s = new SolutionBruteForce();
+        int output = s.maxArea(height);
+        boolean passed = output == 2;
+        System.out.println("test3:" + (passed ? "passed" : "failed"));
+    }
 
-  private static void test8() {
-    int [] height = {2,3,4,5,18,17,6};
-    SolutionTwoPointers s = new SolutionTwoPointers();
-    int output = s.maxArea(height);
-    boolean passed = output == 17;
-    System.out.println("test8:" + (passed ? "passed" : "failed"));
+    private static void test4() {
+        int[] height = { 1, 1 };
+        SolutionTwoPointers s = new SolutionTwoPointers();
+        int output = s.maxArea(height);
+        boolean passed = output == 1;
+        System.out.println("test4:" + (passed ? "passed" : "failed"));
+    }
 
-  }
+    private static void test5() {
+        int[] height = { 4, 3, 2, 1, 4 };
+        SolutionTwoPointers s = new SolutionTwoPointers();
+        int output = s.maxArea(height);
+        boolean passed = output == 16;
+        System.out.println("test5:" + (passed ? "passed" : "failed"));
+    }
 
-  private static void test9() {
-    int [] height = {1,3,2,5,25,24,5};
-    SolutionTwoPointers s = new SolutionTwoPointers();
-    int output = s.maxArea(height);
-    boolean passed = output == 24;
-    System.out.println("test9:" + (passed ? "passed" : "failed"));
+    private static void test6() {
+        int[] height = { 1, 2, 1 };
+        SolutionTwoPointers s = new SolutionTwoPointers();
+        int output = s.maxArea(height);
+        boolean passed = output == 2;
+        System.out.println("test6:" + (passed ? "passed" : "failed"));
+    }
 
-  }
+    private static void test7() {
+        int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        SolutionTwoPointers s = new SolutionTwoPointers();
+        int output = s.maxArea(height);
+        boolean passed = output == 49;
+        System.out.println("test7:" + (passed ? "passed" : "failed"));
+    }
 
-  public static void main(String[] args) {
+    private static void test8() {
+        int[] height = { 2, 3, 4, 5, 18, 17, 6 };
+        SolutionTwoPointers s = new SolutionTwoPointers();
+        int output = s.maxArea(height);
+        boolean passed = output == 17;
+        System.out.println("test8:" + (passed ? "passed" : "failed"));
 
-    test1();
-    test2();
-    test3();
-    test4();
-    test5();
-    test6();
+    }
 
-    test7();
+    private static void test9() {
+        int[] height = { 1, 3, 2, 5, 25, 24, 5 };
+        SolutionTwoPointers s = new SolutionTwoPointers();
+        int output = s.maxArea(height);
+        boolean passed = output == 24;
+        System.out.println("test9:" + (passed ? "passed" : "failed"));
 
-    test8();
-    test9();
-  }
+    }
+
 }
--- algo/leetcode/0011-container-with-most-water/src/com/company/SolutionBruteForce.java
+++ algo/leetcode/0011-container-with-most-water/src/com/company/SolutionBruteForce.java
@@ -1,16 +1,16 @@
 package com.company;
 
 public class SolutionBruteForce {
-  public int maxArea(int[] height) {
-    int maxArea = 0;
-    for (int i = 0; i < height.length; i++) {
-      for (int j = 0; j < i; j++) {
-        int area = Math.min(height[i], height[j]) * (i - j);
-        if (area > maxArea) {
-          maxArea = area;
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int area = Math.min(height[i], height[j]) * (i - j);
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
         }
-      }
+        return maxArea;
     }
-    return maxArea;
-  }
 }
