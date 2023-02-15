c264b3d4c42c6f2202478e4a53a9a6605c13d2f8
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-02-14 22:35:40 -0800

    stole simiplified solution from leetcode solutions graph, problem 153

--- algo/leetcode/0153-find-minimum-in-rotated-sorted-array/src/com/company/Solution.java
+++ algo/leetcode/0153-find-minimum-in-rotated-sorted-array/src/com/company/Solution.java
@@ -3,26 +3,14 @@ package com.company;
 class Solution {
     public int findMin(int[] nums) {
         int l = 0, r = nums.length-1;
-        while (l < r) {
-            if (r-l == 1) {
-                return Math.min(nums[l], nums[r]);
-            }
-            int m = l + (r-l)/2;
-            if (nums[l] <= nums[m] && nums[m] <= nums[r]) {
-                return nums[l];
-            }
-
-            if (nums[l] < nums[m] && nums[m] > nums[r]) {
-                l = m+1;
-                continue;
-            }
-
-            if (nums[l] > nums[m] && nums[m] < nums[r]) {
+        while (l < r-1) {
+            int m = l+(r-l)/2;
+            if (nums[m] > nums[r]) {
+                l = m;
+            } else {
                 r = m;
-                l = l+1;
-                continue;
             }
         }
-        return nums[l];    
+        return Math.min(nums[l], nums[r]);    
     }
 }
