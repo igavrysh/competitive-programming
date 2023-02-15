a5d2ca8b2f15c7b8e01ff82b481ee14022f53695
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-02-15 00:09:12 -0800

    add randmoized solution for problem 169

--- algo/leetcode/0169-majority-element/src/com/company/SolutionRandom.java
+++ algo/leetcode/0169-majority-element/src/com/company/SolutionRandom.java
@@ -1,30 +1,25 @@
 package com.company;
 
-import java.util.HashMap;
+/*
+ * https://github.com/igavrysh/competitive-programming/tree/master/algo/leetcode
+ */ 
 import java.util.Random;
 
 public class SolutionRandom {
     public int majorityElement(int[] nums) {
         int N = nums.length;
-        if (N>=1 && N<=2) {
-            return nums[0];
-        }
         Random rand = new Random();
-        HashMap<Integer, Integer> fq = new HashMap<>();
-        int cnt = (int)(N * 0.75);
-        int k = 0;
-        int maxFq = 0;
-        int maxVal = -1;
-        while (k < cnt) {
-            int i = rand.nextInt(N);
-            int newFq = fq.getOrDefault(nums[i], 0)+1;
-            fq.put(nums[i], newFq);
-            if (newFq > maxFq) {
-                maxFq = newFq;
-                maxVal = nums[i];
+        while (true) {
+            int num = nums[rand.nextInt(N)];
+            int fq = 0;
+            for (int i = 0; i < N; i++) {
+                if (nums[i] == num) {
+                    fq++;
+                }
+                if (fq > N/2) {
+                    return num;
+                }
             }
-            k++;
         }
-        return maxVal;
     }
 }
