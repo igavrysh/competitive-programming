31351d0e4127f22663ac7a461ef84d957e841a4e
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-02-18 09:22:16 -0800

    add practice solution for problem 153

--- algo/leetcode/0153-find-minimum-in-rotated-sorted-array/src/com/company/Main.java
+++ algo/leetcode/0153-find-minimum-in-rotated-sorted-array/src/com/company/Main.java
@@ -6,6 +6,11 @@ class Main {
         testSolution2();
         testSolution3();
         testSolution4();
+
+        testSolutionPractice2_1();
+        testSolutionPractice2_2();
+        testSolutionPractice2_3();
+        testSolutionPractice2_4();
     }
 
     public static void testSolution1() {
@@ -38,5 +43,37 @@ class Main {
         int output = s.findMin(nums);
         boolean passed = output == 1;
         System.out.println("testSolution4: " + (passed ? "passed" : "failed"));
+    }
+
+    public static void testSolutionPractice2_1() {
+        int[] nums = new int[]{3,4,5,1,2};
+        SolutionPractice2 s = new SolutionPractice2();
+        int output = s.findMin(nums);
+        boolean passed = output == 1;
+        System.out.println("testSolutionPractice2_1: " + (passed ? "passed" : "failed"));
+    }
+
+    public static void testSolutionPractice2_2() {
+        int[] nums = new int[]{4,5,6,7,0,1,2};
+        SolutionPractice2 s = new SolutionPractice2();
+        int output = s.findMin(nums);
+        boolean passed = output == 0;
+        System.out.println("testSolutionPractice2_2: " + (passed ? "passed" : "failed"));
+    }
+
+    public static void testSolutionPractice2_3() {
+        int[] nums = new int[]{11,13,15,17};
+        SolutionPractice2 s = new SolutionPractice2();
+        int output = s.findMin(nums);
+        boolean passed = output == 11;
+        System.out.println("testSolutionPractice2_3: " + (passed ? "passed" : "failed"));
+    }
+
+    public static void testSolutionPractice2_4() {
+        int[] nums = new int[]{3,1,2};
+        SolutionPractice2 s = new SolutionPractice2();
+        int output = s.findMin(nums);
+        boolean passed = output == 1;
+        System.out.println("testSolutionPractice2_4: " + (passed ? "passed" : "failed"));
     }
 }
---
+++ algo/leetcode/0153-find-minimum-in-rotated-sorted-array/src/com/company/SolutionPractice2.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class SolutionPractice2 {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length-1;
+        while (l < r) {
+            if (r-l == 1) {
+                return Math.min(nums[l], nums[r]);
+            }
+            int m = l + (r-l)/2;
+            if (nums[l] <= nums[m] && nums[m] <= nums[r]) {
+                return nums[l];
+            }
+
+            if (nums[l] < nums[m] && nums[m] > nums[r]) {
+                l = m+1;
+                continue;
+            }
+
+            if (nums[l] > nums[m] && nums[m] < nums[r]) {
+                r = m;
+                l = l+1;
+                continue;
+            }
+        }
+        return nums[l];    
+    }
+}
