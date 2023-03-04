6260a112501b22559ebd40cf6c875b367625c116
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-03-04 15:20:14 -0800

    practice solution for problem 143

--- algo/leetcode/0143-reorder-list/src/com/company/Main.java
+++ algo/leetcode/0143-reorder-list/src/com/company/Main.java
@@ -2,71 +2,52 @@ package com.company;
 
 public class Main {
 
-  public static void test1() {
-    ListNode input = new ListNode(
-        1,
-        new ListNode(
-            2,
-            new ListNode(
-                3,
-                new ListNode(
-                    4,
-                    null))));
-    Solution s = new Solution();
-    s.reorderList(input);
-    boolean passed = input.val == 1
-        && input.next.val == 4
-        && input.next.next.val == 2
-        && input.next.next.next.val == 3
-        && input.next.next.next.next == null;
-    System.out.println("test1: " + (passed ? "passed" : "failed"));
-  }
+    public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
+        test4();
+    }
 
-  public static void test2() {
-    ListNode input = new ListNode(
-        1,
-        new ListNode(
-            2,
-            new ListNode(
-                3,
-                new ListNode(
-                    4,
-                    new ListNode(
-                        5,
-                        null
-                    )))));
-    Solution s = new Solution();
-    s.reorderList(input);
-    boolean passed = input.val == 1
-        && input.next.val == 5
-        && input.next.next.val == 2
-        && input.next.next.next.val == 4
-        && input.next.next.next.next.val == 3
-        && input.next.next.next.next.next == null;
-    System.out.println("test2: " + (passed ? "passed" : "failed"));
-  }
+    public static void test1() {
+        ListNode input = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,null))));
+        SolutionPractice2 s = new SolutionPractice2();
+        s.reorderList(input);
+        boolean passed = input.val == 1
+                && input.next.val == 4
+                && input.next.next.val == 2
+                && input.next.next.next.val == 3
+                && input.next.next.next.next == null;
+        System.out.println("test1: " + (passed ? "passed" : "failed"));
+    }
 
-  public static void test3() {
-    ListNode input = new ListNode(1, null);
-    Solution s = new Solution();
-    s.reorderList(input);
-    boolean passed = input.val == 1
-        && input.next == null;
-    System.out.println("test3: " + (passed ? "passed" : "failed"));
-  }
+    public static void test2() {
+        ListNode input = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5,null)))));
+        SolutionPractice2 s = new SolutionPractice2();
+        s.reorderList(input);
+        boolean passed = input.val == 1
+                && input.next.val == 5
+                && input.next.next.val == 2
+                && input.next.next.next.val == 4
+                && input.next.next.next.next.val == 3
+                && input.next.next.next.next.next == null;
+        System.out.println("test2: " + (passed ? "passed" : "failed"));
+    }
 
-  public static void test4() {
-    ListNode input = null;
-    Solution s = new Solution();
-    s.reorderList(input);
-    boolean passed = input == null;
-    System.out.println("test4: " + (passed ? "passed" : "failed"));
-  }
+    public static void test3() {
+        ListNode input = new ListNode(1, null);
+        SolutionPractice2 s = new SolutionPractice2();
+        s.reorderList(input);
+        boolean passed = input.val == 1
+                && input.next == null;
+        System.out.println("test3: " + (passed ? "passed" : "failed"));
+    }
 
-  public static void main(String[] args) {
-    test1();
-    test2();
-    test3();
-    test4();
-  }
+    public static void test4() {
+        ListNode input = null;
+        SolutionPractice2 s = new SolutionPractice2();
+        s.reorderList(input);
+        boolean passed = input == null;
+        System.out.println("test4: " + (passed ? "passed" : "failed"));
+    }
 }
