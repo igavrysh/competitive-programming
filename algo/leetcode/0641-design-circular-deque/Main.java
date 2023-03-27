c9effb33f5bcf9bd9be04292241c65192bcf2b41
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-03-27 01:57:33 -0700

    solution for leetcode 641

--- algo/leetcode/0641-design-circular-deque/Main.java
+++ algo/leetcode/0641-design-circular-deque/Main.java
@@ -1,7 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        test1();
     }
     
+    public static void test1() {
+        boolean passed = true;
+        MyCircularDeque myCircularDeque = new MyCircularDeque(3);
+        passed = passed && myCircularDeque.insertLast(1);  // return True
+        passed = passed && myCircularDeque.insertLast(2);  // return True
+        passed = passed && myCircularDeque.insertFront(3); // return True
+        passed = passed && !myCircularDeque.insertFront(4); // return False, the queue is full.
+        passed = passed && myCircularDeque.getRear() == 2;      // return 2
+        passed = passed && myCircularDeque.isFull();       // return True
+        passed = passed && myCircularDeque.deleteLast();   // return True
+        passed = passed && myCircularDeque.insertFront(4); // return True
+        passed = passed && myCircularDeque.getFront() == 4;     // return 4
+        System.out.println("test1: " + (passed ? "passed" : "failed"));
+    }
 }
--- algo/leetcode/0641-design-circular-deque/MyCircularDeque.java
+++ algo/leetcode/0641-design-circular-deque/MyCircularDeque.java
@@ -1,39 +1,86 @@
 public class MyCircularDeque {
 
+    private int capacity;
+    private int size;
+
+    private Node sentFront;
+    private Node sentLast;
+    
+    public class Node {
+        int value;
+        Node next;
+        Node prev;
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
     public MyCircularDeque(int k) {
-        
+        capacity = k;
+        sentLast = new Node(-1, null, null);
+        sentFront = new Node(-1, sentLast, null);
+        sentLast.prev = sentFront;
     }
     
     public boolean insertFront(int value) {
-        return false;
+        if (isFull()) {
+            return false;
+        }
+        Node node = new Node(value, sentFront.next, sentFront);
+        node.next.prev = node;
+        sentFront.next = node;
+        size++;
+        return true;
     }
     
     public boolean insertLast(int value) {
-        return false;
+        if (isFull()) {
+            return false;
+        }
+        Node node = new Node(value, sentLast, sentLast.prev);
+        sentLast.prev = node;
+        node.prev.next = node;
+        size++;
+        return true;
     }
     
     public boolean deleteFront() {
-        return false;
+        if (isEmpty()) {
+            return false;
+        }
+        Node deleteNode = sentFront.next;
+        sentFront.next = deleteNode.next;
+        deleteNode.next.prev = sentFront;
+        size--;
+        return true;
     }
     
     public boolean deleteLast() {
-        return false;
+        if (isEmpty()) {
+            return false;
+        }
+        Node nodeDelete = sentLast.prev;
+        nodeDelete.prev.next = sentLast;
+        sentLast.prev = nodeDelete.prev;
+        size--;
+        return true;
     }
     
     public int getFront() {
-        return -1;
+        return sentFront.next.value;
     }
     
     public int getRear() {
-        return -1;
+        return sentLast.prev.value;
     }
     
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
     
     public boolean isFull() {
-        return false;
+        return size == capacity;
     }
-    
 }
