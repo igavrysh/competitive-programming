aa4f5dc7f855917c8290f73b60ba612a7dd5e57e
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-03-31 20:05:05 -0700

    solution for problem 1046 leetcode

--- algo/leetcode/1046-last-stone-weight/Main.java
+++ algo/leetcode/1046-last-stone-weight/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        testSolutionInplace1();
+    }
+
+    public static void testSolutionInplace1() {
+        int[] stones = new int[]{2,7,4,1,8,1};
+        SolutionInplace s = new SolutionInplace();
+        int output = s.lastStoneWeight(stones);
+        boolean passed = output == 1; 
+        System.out.println("testSolutionInplace1: " + (passed ? "passed" : "failed")); 
     }
 }
--- algo/leetcode/1046-last-stone-weight/SolutionInPlace.java
+++ algo/leetcode/1046-last-stone-weight/SolutionInPlace.java
@@ -1,3 +1,66 @@
-public class SolutionInPlace {
-    
+class SolutionInplace {
+
+    private void siftdown(int i, int length, int[] a) {
+        while (2*i-1<length && 2*i-1>=0) {
+            int j = i;
+            if (a[2*i-1] > a[j]) {
+                j = 2*i-1;
+            }
+
+            if (2*i<length && a[2*i] > a[j]) {
+                j = 2*i;
+            }
+
+            if (i==j) {
+                break;
+            }
+
+            swap(i, j, a);
+
+            i = j;
+        }
+    }
+
+    private void siftup(int i, int[] a) {
+        while (i/2-1>=0 && a[i/2-1]<a[i]) {
+            swap(i/2-1, i, a);
+            i = i/2-1;
+        }
+    } 
+
+    private void swap(int i, int j, int[] a) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    private void heapify(int[] a) {
+        int n = a.length;
+        for (int i = n/2-1; i>=0; i--) {
+            siftdown(i, n, a);
+        }
+    }
+
+    public int lastStoneWeight(int[] stones) {
+        heapify(stones);
+        int length = stones.length;
+        while (length > 1) {
+            int stone1 = stones[0];
+            length--;
+            stones[0] = stones[length];
+            siftdown(0, length, stones);
+            int stone2 = stones[1];
+
+            if (stone1 != stone2) {
+                stones[0] = stone1-stone2;
+                siftdown(0, length, stones);
+            } else {
+                length--;
+                stones[0] = stones[length];
+                siftdown(0, length, stones);
+            }
+        }
+        return length == 0 ? 0 : stones[0];
+    }
+
 }
