5cfb9922f8a58118a6da2f60e56edd745452215c
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-04-01 07:49:23 -0700

    minor modifications task 973 leetcode

--- algo/leetcode/0973-k-closest-points-to-origin/src/com/company/Solution.java
+++ algo/leetcode/0973-k-closest-points-to-origin/src/com/company/Solution.java
@@ -3,66 +3,65 @@ package com.company;
 import java.util.Random;
 
 public class Solution {
-  private void swap(int i, int j, int [][]points) {
-    int[] tmp = points[i];
-    points[i] = points[j];
-    points[j] = tmp;
-  }
-
-  public int[][] kClosest(int[][] points, int k) {
-    quickSelect(k, 0, points.length-1, points);
-    int[][] output = new int[k][2];
-    for (int i = 0; i < k; i++) {
-      output[i] = points[i];
-    }
-    return output;
-  }
-
-  private int partition(int l, int r, int pivot, int[][] points) {
-    if (l == r) {
-      return l;
+    private void swap(int i, int j, int[][] points) {
+        int[] tmp = points[i];
+        points[i] = points[j];
+        points[j] = tmp;
     }
 
-    double distPivot = dist(points[pivot]);
-
-    // move pivot to the right
-    swap(pivot, r, points);
-
-    // move smaller points to the left
-    int storeIndex = l;
-    for (int i = l; i < r; i++) {
-      if (dist(points[i]) < distPivot) {
-        swap(storeIndex, i, points);
-        storeIndex++;
-      }
+    public int[][] kClosest(int[][] points, int k) {
+        quickSelect(k, 0, points.length - 1, points);
+        int[][] output = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            output[i] = points[i];
+        }
+        return output;
     }
 
-    // put pivot to its final place
-    swap(r, storeIndex, points);
-    return storeIndex;
-  }
+    private int partition(int l, int r, int pivot, int[][] points) {
+        if (l == r) {
+            return l;
+        }
 
-  private void quickSelect(int k, int l, int r, int[][] points) {
-    Random rand = new Random();
+        double distPivot = dist(points[pivot]);
 
-    int pivotIdx = l + rand.nextInt(r-l+1);
+        // move pivot to the right
+        swap(pivot, r, points);
 
-    int pos = partition(l, r, pivotIdx, points);
+        // move smaller points to the left
+        int storeIndex = l;
+        for (int i = l; i < r; i++) {
+            if (dist(points[i]) < distPivot) {
+                swap(storeIndex, i, points);
+                storeIndex++;
+            }
+        }
 
-    if (pos == k-1) {
-      return;
-    } else if (pos > k-1) {
-      r = pos-1;
-      quickSelect(k, l, r, points);
-    } else {
-      l = pos+1;
-      quickSelect(k, l, r, points);
+        // put pivot to its final place
+        swap(r, storeIndex, points);
+        return storeIndex;
     }
-  }
 
-  private double dist(int[] point) {
-    return Math.pow(point[0], 2) + Math.pow(point[1], 2);
-  }
+    private void quickSelect(int k, int l, int r, int[][] points) {
+        Random rand = new Random();
 
+        int pivotIdx = l + rand.nextInt(r - l + 1);
+
+        int pos = partition(l, r, pivotIdx, points);
+
+        if (pos == k - 1) {
+            return;
+        } else if (pos > k - 1) {
+            r = pos - 1;
+            quickSelect(k, l, r, points);
+        } else {
+            l = pos + 1;
+            quickSelect(k, l, r, points);
+        }
+    }
+
+    private double dist(int[] point) {
+        return Math.pow(point[0], 2) + Math.pow(point[1], 2);
+    }
 
 }
---
+++ algo/leetcode/0973-k-closest-points-to-origin/src/com/company/SolutionHeap.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class SolutionHeap {
+    public int[][] kClosest(int[][] points, int k) {
+        
+    }
+}
