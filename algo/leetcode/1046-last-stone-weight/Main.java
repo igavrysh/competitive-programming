a2b80872f70828603d94ae6345161bc41b77c124
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-03-31 21:05:45 -0700

    add inplace solution for 1046 leetcode

--- algo/leetcode/1046-last-stone-weight/Main.java
+++ algo/leetcode/1046-last-stone-weight/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         testSolutionInplace1();
+        testSolutionInplace2();
+        testSolutionInplace3();
     }
 
     public static void testSolutionInplace1() {
@@ -9,5 +11,21 @@ public class Main {
         int output = s.lastStoneWeight(stones);
         boolean passed = output == 1; 
         System.out.println("testSolutionInplace1: " + (passed ? "passed" : "failed")); 
+    }
+
+    public static void testSolutionInplace2() {
+        int[] stones = new int[]{1};
+        SolutionInplace s = new SolutionInplace();
+        int output = s.lastStoneWeight(stones);
+        boolean passed = output == 1; 
+        System.out.println("testSolutionInplace2: " + (passed ? "passed" : "failed")); 
+    }
+
+    public static void testSolutionInplace3() {
+        int[] stones = new int[]{3,7,2};
+        SolutionInplace s = new SolutionInplace();
+        int output = s.lastStoneWeight(stones);
+        boolean passed = output == 2; 
+        System.out.println("testSolutionInplace3: " + (passed ? "passed" : "failed")); 
     }
 }
--- algo/leetcode/1046-last-stone-weight/SolutionInPlace.java
+++ algo/leetcode/1046-last-stone-weight/SolutionInPlace.java
@@ -1,14 +1,15 @@
 class SolutionInplace {
-
+    // child(i) = (i+1)*2-1, (i+1)*2 
+    // parent(i) = (i-1)/2
     private void siftdown(int i, int length, int[] a) {
-        while (2*i-1<length && 2*i-1>=0) {
+        while ((i+1)*2-1<length && (i+1)*2-1>=0) {
             int j = i;
-            if (a[2*i-1] > a[j]) {
-                j = 2*i-1;
+            if (a[(i+1)*2-1] > a[j]) {
+                j = (i+1)*2-1;
             }
 
-            if (2*i<length && a[2*i] > a[j]) {
-                j = 2*i;
+            if ((i+1)*2<length && a[(i+1)*2] > a[j]) {
+                j = (i+1)*2;
             }
 
             if (i==j) {
@@ -20,13 +21,6 @@ class SolutionInplace {
             i = j;
         }
     }
-
-    private void siftup(int i, int[] a) {
-        while (i/2-1>=0 && a[i/2-1]<a[i]) {
-            swap(i/2-1, i, a);
-            i = i/2-1;
-        }
-    } 
 
     private void swap(int i, int j, int[] a) {
         int tmp = a[i];
@@ -49,7 +43,7 @@ class SolutionInplace {
             length--;
             stones[0] = stones[length];
             siftdown(0, length, stones);
-            int stone2 = stones[1];
+            int stone2 = stones[0];
 
             if (stone1 != stone2) {
                 stones[0] = stone1-stone2;
@@ -62,5 +56,4 @@ class SolutionInplace {
         }
         return length == 0 ? 0 : stones[0];
     }
-
 }
