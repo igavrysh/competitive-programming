42639da4e70759be707c77b081dc5a7385baf885
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-04-01 22:29:38 -0700

    973 pq solution leetcode

--- algo/leetcode/0973-k-closest-points-to-origin/src/com/company/Main.java
+++ algo/leetcode/0973-k-closest-points-to-origin/src/com/company/Main.java
@@ -9,6 +9,9 @@ public class Main {
         
         testSolutionHeap1();
         testSolutionHeap2();
+
+        testSolutionPQ1();
+        testSolutionPQ2();
     }
 
     public static void test1() {
@@ -47,4 +50,36 @@ public class Main {
         System.out.println("testSolutionHeap2: " + (passed ? "passed" : "failed"));
     }
 
+    public static void testSolutionPQ1() {
+        int[][] input = { { 1, 3 }, { -2, 2 } };
+        int k = 1;
+        SolutionPQ s = new SolutionPQ();
+        int[][] output = s.kClosest(input, k);
+        boolean passed = output.length == 1 && Arrays.equals(output[0], new int[] { -2, 2 });
+        System.out.println("testSolutionPQ1: " + (passed ? "passed" : "failed"));
+    }
+
+    public static void testSolutionPQ2() {
+        int[][] input = { { 3, 3 }, { 5, -1 }, { -2, 4 } };
+        int k = 2;
+        SolutionPQ s = new SolutionPQ();
+        int[][] output = s.kClosest(input, k);
+        int[][] expectedOutput = new int[][] { { 3, 3 }, { -2, 4 } };
+        
+        boolean passed = true;
+
+        for (int i = 0; i < output.length; i++) {
+            boolean matched = false;
+            for (int j = 0; j < expectedOutput.length; j++) {
+                if (Arrays.equals(output[i], expectedOutput[j])) {
+                    matched = true;
+                }
+            }
+
+            if (!matched) {
+                passed = false;
+            }
+        }
+        System.out.println("testSolutionPQ2: " + (passed ? "passed" : "failed"));
+    }
 }
--- algo/leetcode/0973-k-closest-points-to-origin/src/com/company/SolutionPQ.java
+++ algo/leetcode/0973-k-closest-points-to-origin/src/com/company/SolutionPQ.java
@@ -1,5 +1,30 @@
 package com.company;
 
-public class SolutionPQ {
-    
+import java.util.PriorityQueue;
+
+class SolutionPQ {
+    private double distSq(int[] p) {
+        return Math.pow(p[0], 2) + Math.pow(p[1], 2);
+    }
+
+    public int[][] kClosest(int[][] points, int k) {
+        // max heap
+        PriorityQueue<int[]> PQ = new PriorityQueue<>((p1, p2) -> {
+            double d1 = distSq(p1), d2 = distSq(p2);
+            return d1==d2 ? 0 : (d2 < d1 ? -1 : 1);
+        });
+
+        for (int i = 0; i < points.length; i++) {
+            PQ.add(points[i]);
+            if (PQ.size() == k+1) {
+                PQ.poll();
+            }
+        }
+
+        int[][] output = new int[PQ.size()][2];
+        for (int i = 0; i < output.length; i++) {
+            output[i] = PQ.poll();
+        }
+        return output;
+    }
 }
