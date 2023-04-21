24cf27b7312789c34984786d03e0b7933a8aa383
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-04-21 06:34:30 -0700

    fixed few issues with leetcode products matrix solution

--- algo/hackerrank/products-matrix/Solution.java
+++ algo/hackerrank/products-matrix/Solution.java
@@ -4,21 +4,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
 
     private static class Args {
         public int n;
-        public int k;
-        public Args(int n, int k) {
+        public long k;
+        public Args(int n, long k) {
             this.n = n;
             this.k = k;
         }
     }
 
     public static void main(String[] args) throws IOException {
-        
+        /* 
         testSolution_0();
         testSolution_1();
         testSolution_2();
@@ -26,29 +27,26 @@ public class Solution {
         testSolution_4();
         testSolution_4abc();
         testSolution_4abcd();
-    
-        //testSolution_5();
-
-        /* 
+        for (int i = 6; i <= 36; i++) {
+            testSolution_4_gen(i);
+        }
+        testSolution_5();
+        */
         Args nkArgs = readInput();
         Solution solution = new Solution();
-        int output = solution.kthElement(nkArgs.n, nkArgs.k);
+        long output = solution.kthElement(nkArgs.n, nkArgs.k);
         writeOutput(output);
-        */
     }
 
     public long kthElement(int n, long k) {
         long[] DP = new long[n+1];
         DP[0] = 0; DP[1] = 1;
-        int startI = 1;
         for (int j = 2; j <= n; j++) {
             for (int i = 1; i <= j; i++) {
-                startI = (int)Math.sqrt((long)i*j);
-                if (i*j<=(startI*startI)) {
-                    DP[startI]++;
-                } else {
-                    DP[startI+1]++;
-                }
+                long val = ((long)i) * j;
+                long valSqrt = (long) Math.sqrt(val);
+                int bucket = (int)valSqrt + (valSqrt * valSqrt == val ? 0 : 1);
+                DP[bucket]++;
             }
         }
 
@@ -57,7 +55,6 @@ public class Solution {
         for (int i = 1; i < accDP.length; i++) {
             accDP[i] = accDP[i-1] + (2*DP[i]-1);
         }
-
         
         int bad = 0, good = n+1;
         while (good-bad>1) {
@@ -70,34 +67,23 @@ public class Solution {
         }
 
         List<Long> arr = new ArrayList<>(); 
-        long j = good;
-        //long i = good;
-        for (long i = good; i <= n; i++) {
-        //while (true) {
-            
-            boolean addeded = false;
-            long upperBound = ((long)good)*good;
-            long lowerBound = ((long)good-1) *(good-1);
-
-            long i_j = i*j;
-            if (i_j <= upperBound && i_j > lowerBound) {
-               arr.add(i_j);
-               addeded = true;
+        long i = good;
+        long upperBound = ((long)good)*good;
+        long lowerBound = ((long)good-1)*(good-1);
+        boolean movePossible = true;
+        arr.add(i*i);
+        i--;
+        while (movePossible && i>0) {
+            movePossible = false;
+            long maxJ = Math.min((long)upperBound/i, n);
+            for (long j = (long)lowerBound/i; j <= maxJ ; j++) {
+                long ij = i * j;
+                if (ij > lowerBound && ij <= upperBound) {
+                    arr.add(ij);
+                    movePossible = true;
+                }
             }
-
-
-            long i_j_1 = i*(j-1);
-            if (j>1 && i_j_1 <= upperBound && i_j_1 > lowerBound) {
-                arr.add(i_j_1);
-                j--;
-                addeded = true;
-            }
-
-            if (!addeded) {
-                break;
-            }
-
-            //i++;
+            i--;
         }
 
         arr.sort((i1, i2) -> i1 == i2 ? 0 : (i1 > i2 ? 1 : -1));
@@ -112,7 +98,7 @@ public class Solution {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String[] firstLine = readLineNSpaceSeparate(br);
             int n = Integer.parseInt(firstLine[0]);
-            int k = Integer.parseInt(firstLine[1]);
+            long k = Long.parseLong(firstLine[1]);
             args = new Args(n, k);
         }
         return args;
@@ -122,7 +108,7 @@ public class Solution {
         return r.readLine().replaceAll("\\s+$", "").split(" ");
     }
 
-    private static void writeOutput(int output) throws IOException {
+    private static void writeOutput(long output) throws IOException {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out))) {
             String outputStr = String.valueOf(output); 
             bufferedWriter.write(outputStr);
@@ -191,12 +177,15 @@ public class Solution {
     }
 
     public static void testSolution_4abcd() {
-        int n = 6;
+        int n = 5;
         Solution s = new Solution();
         int[] inputK =  {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
         long[] output = {1,2,2,3,3,4,4,4,5, 5, 6, 6, 8, 8, 9,10,10,12,12,15,15,16,20,20,25};
         int passedCasses = 0;
         for (int i = 0; i < inputK.length; i++) {
+            if (inputK[i] == 20) {
+                int t = 1;
+            }
             long outputI = s.kthElement(n, inputK[i]);
             boolean passed = outputI == output[i];
             if (!passed) {
@@ -214,8 +203,44 @@ public class Solution {
         int n = 100_000, k = 1_000_000_000;
         Solution s = new Solution();
         long output = s.kthElement(n, k);
-        boolean passed = output == 3;
+        boolean passed = output == 204535821;
         System.out.println("testSolution_5: " + (passed ? "passed" : "failed")); 
+    }
 
+    public static void testSolution_4_gen(int n) {
+        Solution s = new Solution();
+
+        int[] inputK = new int[n*n];
+        for (int i = 0; i < inputK.length; i++) {
+            inputK[i] = i+1;
+        }
+
+        long[] output = new long[n*n];
+        int idx=0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                output[idx++] = i*j;
+            }
+        }
+
+        Arrays.sort(output);
+
+        int passedCasses = 0;
+
+        for (int i = 0; i < inputK.length; i++) {
+            if (inputK[i] == 15) {
+                int t = 1;
+            }
+            long outputI = s.kthElement(n, inputK[i]);
+            boolean passed = outputI == output[i];
+            if (!passed) {
+                System.out.println("testSolution_4_gen:(" + n + "): " + " k = " + inputK[i] + "; " + (passed ? "passed" : "failed"));
+            } else {
+                passedCasses++;
+            }
+        }
+        
+        boolean passed = passedCasses == output.length;
+        System.out.println("testSolution_4_gen (" + n + "): "  + (passed ? "passed" : "failed"));
     }
 }
