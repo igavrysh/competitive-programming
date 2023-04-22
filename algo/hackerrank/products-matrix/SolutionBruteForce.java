1936ad0b6f14cc9f0e3ac46e5f93fd5cd33b8332
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-04-22 14:02:03 -0700

    bs solution for hackerrank products matrix

--- algo/hackerrank/products-matrix/SolutionBruteForce.java
+++ algo/hackerrank/products-matrix/SolutionBruteForce.java
@@ -3,9 +3,11 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class Solution {
+public class SolutionBruteForce {
 
     private static class Args {
         public int n;
@@ -17,46 +19,79 @@ public class Solution {
     }
 
     public static void main(String[] args) throws IOException {
-        /*
-        testSolutionBS_0();
-        testSolutionBS_1();
-        testSolutionBS_2();
-        testSolutionBS_3();
-        testSolutionBS_4();
-        testSolutionBS_4abc();
-        testSolutionBS_4abcd();
+        testSolution_0();
+        testSolution_1();
+        testSolution_2();
+        testSolution_3();
+        testSolution_4();
+        testSolution_4abc();
+        testSolution_4abcd();
         for (int i = 6; i <= 36; i++) {
-            testSolutionBS_4_gen(i);
+            testSolution_4_gen(i);
         }
-        testSolutionBS_5();
-        */
+        //testSolution_5();
         
+        /* 
         Args nkArgs = readInput();
         Solution solution = new Solution();
         long output = solution.kthElement(nkArgs.n, nkArgs.k);
-        writeOutput(output);    
+        writeOutput(output);
+        */
     }
 
-    private boolean isEnough(long val, int n, long k) {
-        long accK = 0;
-        for (int i = n; i >= 1; i--) {
-            accK += Math.min(val/i, n);
-            if (accK >= k) { return true; }
+    public long kthElement(int n, long k) {
+        long[] DP = new long[n+1];
+        DP[0] = 0; DP[1] = 1;
+        for (int j = 2; j <= n; j++) {
+            for (int i = 1; i <= j; i++) {
+                long val = ((long)i) * j;
+                long valSqrt = (long) Math.sqrt(val);
+                int bucket = (int)valSqrt + (valSqrt * valSqrt == val ? 0 : 1);
+                DP[bucket]++;
+            }
         }
-        return false;
-    }
 
-    public long kthElement(int n, long k) {    
-        long bad = 0, good = (long)n*n+1;
+        long[] accDP = new long[n+1];
+        accDP[0] = 0;
+        for (int i = 1; i < accDP.length; i++) {
+            accDP[i] = accDP[i-1] + (2*DP[i]-1);
+        }
+        
+        int bad = 0, good = n+1;
         while (good-bad>1) {
-            long m = bad+(good-bad)/2;
-            if (isEnough(m, n, k)) {
+            int m = bad+(good-bad)/2;
+            if (accDP[m] >= k) {
                 good = m;
             } else {
                 bad = m;
             }
         }
-        return good;
+
+        List<Long> arr = new ArrayList<>(); 
+        long i = good;
+        long upperBound = ((long)good)*good;
+        long lowerBound = ((long)good-1)*(good-1);
+        boolean movePossible = true;
+        arr.add(i*i);
+        i--;
+        while (movePossible && i>0) {
+            movePossible = false;
+            long maxJ = Math.min((long)upperBound/i, n);
+            for (long j = (long)lowerBound/i; j <= maxJ ; j++) {
+                long ij = i * j;
+                if (ij > lowerBound && ij <= upperBound) {
+                    arr.add(ij);
+                    movePossible = true;
+                }
+            }
+            i--;
+        }
+
+        arr.sort((i1, i2) -> i1 == i2 ? 0 : (i1 > i2 ? 1 : -1));
+
+        int incrK = (int)(k - accDP[good-1]);
+
+        return arr.get((incrK-1)/2);
     }
 
     private static Args readInput() throws IOException {
@@ -82,49 +117,49 @@ public class Solution {
         }
     }
 
-    public static void testSolutionBS_0() {
+    public static void testSolution_0() {
         int n = 3, k = 7;
-        Solution s = new Solution();
+        SolutionBruteForce s = new SolutionBruteForce();
         long output = s.kthElement(n, k);
         boolean passed = output == 6;
-        System.out.println("testSolutionBS_0: " + (passed ? "passed" : "failed"));
+        System.out.println("testSolution_0: " + (passed ? "passed" : "failed"));
     }
 
-    public static void testSolutionBS_1() {
+    public static void testSolution_1() {
         int n = 2, k = 4;
-        Solution s = new Solution();
+        SolutionBruteForce s = new SolutionBruteForce();
         long output = s.kthElement(n, k);
         boolean passed = output == 4;
-        System.out.println("testSolutionBS_1: " + (passed ? "passed" : "failed"));
+        System.out.println("testSolution_1: " + (passed ? "passed" : "failed"));
     }
 
-    public static void testSolutionBS_2() {
+    public static void testSolution_2() {
         int n = 3, k = 8;
-        Solution s = new Solution();
+        SolutionBruteForce s = new SolutionBruteForce();
         long output = s.kthElement(n, k);
         boolean passed = output == 6;
-        System.out.println("testSolutionBS_2: " + (passed ? "passed" : "failed"));    
+        System.out.println("testSolution_2: " + (passed ? "passed" : "failed"));    
     }
 
-    public static void testSolutionBS_3() {
+    public static void testSolution_3() {
         int n = 1, k = 1;
-        Solution s = new Solution();
+        SolutionBruteForce s = new SolutionBruteForce();
         long output = s.kthElement(n, k);
         boolean passed = output == 1;
-        System.out.println("testSolutionBS_3: " + (passed ? "passed" : "failed"));    
+        System.out.println("testSolution_3: " + (passed ? "passed" : "failed"));    
     }
 
-    public static void testSolutionBS_4() {
+    public static void testSolution_4() {
         int n = 4, k = 4;
-        Solution s = new Solution();
+        SolutionBruteForce s = new SolutionBruteForce();
         long output = s.kthElement(n, k);
         boolean passed = output == 3;
-        System.out.println("testSolutionBS_4: " + (passed ? "passed" : "failed"));    
+        System.out.println("testSolution_4: " + (passed ? "passed" : "failed"));    
     }
 
-    public static void testSolutionBS_4abc() {
+    public static void testSolution_4abc() {
         int n = 4;
-        Solution s = new Solution();
+        SolutionBruteForce s = new SolutionBruteForce();
         int[] inputK = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
         long[] output = {1,2,2,3,3,4,4,4,6, 6, 8, 8, 9,12,12,16};
         int passedCasses = 0;
@@ -132,19 +167,19 @@ public class Solution {
             long outputI = s.kthElement(n, inputK[i]);
             boolean passed = outputI == output[i];
             if (!passed) {
-                System.out.println("testSolutionBS_4abc: k = " + inputK[i] + "; " + (passed ? "passed" : "failed"));
+                System.out.println("testSolution_4abc: k = " + inputK[i] + "; " + (passed ? "passed" : "failed"));
             } else {
                 passedCasses++;
             }
         }
         
         boolean passed = passedCasses == output.length;
-        System.out.println("testSolutionBS_4abc: " + (passed ? "passed" : "failed"));
+        System.out.println("testSolution_4abc: " + (passed ? "passed" : "failed"));
     }
 
-    public static void testSolutionBS_4abcd() {
+    public static void testSolution_4abcd() {
         int n = 5;
-        Solution s = new Solution();
+        SolutionBruteForce s = new SolutionBruteForce();
         int[] inputK =  {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
         long[] output = {1,2,2,3,3,4,4,4,5, 5, 6, 6, 8, 8, 9,10,10,12,12,15,15,16,20,20,25};
         int passedCasses = 0;
@@ -162,19 +197,19 @@ public class Solution {
         }
         
         boolean passed = passedCasses == output.length;
-        System.out.println("testSolutionBS_4abcdg: " + (passed ? "passed" : "failed"));
+        System.out.println("testSolution_4abcdg: " + (passed ? "passed" : "failed"));
     }
 
-    public static void testSolutionBS_5() {
+    public static void testSolution_5() {
         int n = 100_000, k = 1_000_000_000;
-        Solution s = new Solution();
+        SolutionBruteForce s = new SolutionBruteForce();
         long output = s.kthElement(n, k);
         boolean passed = output == 204535821;
-        System.out.println("testSolutionBS_5: " + (passed ? "passed" : "failed")); 
+        System.out.println("testSolution_5: " + (passed ? "passed" : "failed")); 
     }
 
-    public static void testSolutionBS_4_gen(int n) {
-        Solution s = new Solution();
+    public static void testSolution_4_gen(int n) {
+        SolutionBruteForce s = new SolutionBruteForce();
 
         int[] inputK = new int[n*n];
         for (int i = 0; i < inputK.length; i++) {
@@ -200,13 +235,13 @@ public class Solution {
             long outputI = s.kthElement(n, inputK[i]);
             boolean passed = outputI == output[i];
             if (!passed) {
-                System.out.println("testSolutionBS_4_gen:(" + n + "): " + " k = " + inputK[i] + "; " + (passed ? "passed" : "failed"));
+                System.out.println("testSolution_4_gen:(" + n + "): " + " k = " + inputK[i] + "; " + (passed ? "passed" : "failed"));
             } else {
                 passedCasses++;
             }
         }
         
         boolean passed = passedCasses == output.length;
-        System.out.println("testSolutionBS_4_gen (" + n + "): "  + (passed ? "passed" : "failed"));
+        System.out.println("testSolution_4_gen (" + n + "): "  + (passed ? "passed" : "failed"));
     }
 }
