9c74082991d4ef7cbb0f645f4e39f023a466674d
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-09-21 05:10:45 -0700

    solution leetcode 334

--- algo/leetcode/0334-increasing-triplet-subsequence/Solution5Dp.java
+++ algo/leetcode/0334-increasing-triplet-subsequence/Solution5Dp.java
@@ -3,16 +3,8 @@
 
 public class Solution5Dp {
     public boolean increasingTriplet(int[] nums) {
-        // dp[l] = index of minimal last minimal element of strictly increasing sequence of length l
-        // update dp[1], dp[2], to maintain invariant - as you progress thru array of nums, e.g. 
-        // e.g. with arrival of nums[i] there are 2 possibilies of for the sequence of length = 2
-        // 1) nums[i] does not change anything, e.g. increasing sequence with a length of 2 does not 
-        //  change its minimal tail, as its tail (nums[dp[2]]) is less than new elem (nums[i]))
-        // 2) nums[i] changes the sequence of length 2, as it becomes a new minimal tail for sequence of length 2, 
-        //  the following conditions should hold:
-        //      1) new tail should be less than previous tail for seq of length 2: nums[i] < nums[dp[2]]
-        //      2) new tail should be a "valid" tail: new tail value should be greater than minimal tail of sequence of length 1:
-        //          nums[dp[1]] < nums[i] 
+        // dp[L] = stores indexe of minimal tail element of strictly increasing sequence of length L
+        // update dp[1], dp[2], to maintain invariant - as you progress thru array of nums
         int[] dp = new int[3];
         dp[0] = -1; // just filler - not used
         dp[1] = 0;
@@ -20,14 +12,26 @@ public class Solution5Dp {
 
         for (int i = 1; i < nums.length; i++) {
             // update for seq length = 1
+            // is the new tail of sequence of length 1 less than a previous tail for sequence of length 1? 
+            // tivial case
             if (nums[dp[1]] > nums[i]) {
                 dp[1] = i;
             }
 
+            // with arrival of nums[i] there are 2 possibilies of for the sequence of length = 2
+            // 1) nums[i] does not change anything, e.g. increasing sequence with a length of 2 does not 
+            //  change its minimal tail, as its tail (nums[dp[2]]) is less than new elem (nums[i]))
+            // 2) nums[i] changes the sequence of length 2, as it becomes a new minimal tail for sequence of length 2, 
+            //  the following conditions should hold:
+            //      1) new tail should be less than previous tail for seq of length 2: nums[i] < nums[dp[2]]
+            //      2) new tail should be a "valid" tail: new tail value should be greater than minimal tail of sequence of length 1:
+            //          nums[dp[1]] < nums[i] 
             if ((dp[2] == -1 || nums[dp[2]] > nums[i]) && nums[dp[1]] < nums[i]) {
                 dp[2] = i;
             }
 
+            // can a current value be a new tail of sequence of length 3? Drop minimality condition, just use "validity" check as we need
+            // to answer if the seq of length 3 exists
             if (dp[2] != -1 && nums[dp[2]] < nums[i]) {
                 return true;
             }
