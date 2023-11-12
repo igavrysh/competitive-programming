ce1d52a8d3083ebf88c4cdd9ea0cd4c6897fbe7b
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-11-12 09:30:34 -0800

    add problem statement for uniform integers

---
+++ algo/test/fb/uniform-integers/Solution.java
@@ -0,0 +1,9 @@
+// Write any import statements here
+
+class Solution {
+
+    public int getUniformIntegerCountInInterval(long A, long B) {
+        // Write your code here
+        return 0;
+    }
+}
