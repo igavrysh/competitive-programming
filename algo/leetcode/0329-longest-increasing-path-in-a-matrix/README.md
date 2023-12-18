9fafbab2c1123564a74cc3e1e1f0f4e30fc34208
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-12-18 13:35:08 -0800

    error somewhere...

--- algo/leetcode/0329-longest-increasing-path-in-a-matrix/README.md
+++ algo/leetcode/0329-longest-increasing-path-in-a-matrix/README.md
@@ -4,11 +4,12 @@ https://leetcode.com/problems/longest-increasing-path-in-a-matrix
 
 `Hard`
 
-Given an m x n integers matrix, return the length of the longest increasing path in matrix.
+Given an `m x n` integers matrix, return the length of the longest increasing path in matrix.
 
 From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
 
 **Example 1**:
+
 ![ex1](ex1.jpg)
 ```
 Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
@@ -17,6 +18,7 @@ Explanation: The longest increasing path is [1, 2, 6, 9].
 ```
 
 **Example 2**:
+
 ![ex2](ex2.jpg)
 ```
 Input: matrix = [[3,4,5],[3,2,6],[2,2,1]]
--- algo/leetcode/0329-longest-increasing-path-in-a-matrix/Solution.java
+++ algo/leetcode/0329-longest-increasing-path-in-a-matrix/Solution.java
@@ -1,5 +1,116 @@
+import java.util.Stack;
+
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        return 1;
+        int R = matrix.length;
+        int C = matrix[0].length;
+        long[] dis = new long[R*C+2];
+        for (int i = 0; i < R*C; i++) {
+            dis[i] = -1;
+        }
+        dis[R*C] = 0;
+        dis[R*C+1] = -2;
+
+        Stack<Integer> order = postOrder(R, C, matrix);
+        while (!order.isEmpty()) {
+            Integer curr = order.pop();
+            if (curr == R*C) { 
+                continue; 
+            }
+            if (curr == R * C + 1) {
+                for (int i = 0; i < R*C; i++) {
+                    if (dis[curr] > dis[i]-1) {
+                        dis[curr] = dis[i]-1;
+                    }
+                }
+                continue;
+            }
+
+            int r = curr / C;
+            int c = curr % C;
+            for (int d = 0; d < dirs.length; d++) {
+                int fromR = r + dirs[d][0];
+                int fromC = c + dirs[d][1];
+                if (!(fromR >= 0 && fromR < R && fromC >= 0 && fromC < C)) {
+                    continue;
+                }
+
+                if (matrix[fromR][fromC] >= matrix[r][c]) {
+                    continue;
+                }
+
+                if (dis[r*C+c] > dis[fromR*C+fromC]-1) {
+                    dis[r*C+c] = dis[fromR*C+fromC]-1;
+                }
+            }
+        }
+        return -1*(int)(dis[R*C+1])-1;
+    }
+
+    private Stack<Integer> postOrder(int R, int C, int[][] matrix) {
+        boolean[] marked = new boolean[R*C+2];
+        Stack<Integer> order = new Stack<>();
+        for (int i = 0; i < marked.length; i++) {
+            if (!marked[i]) {
+                dfs(i, marked, order, R, C, matrix);
+            }
+        }
+        return order;
+    }
+
+    private static int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+    private void dfs(int curr, boolean[] marked, Stack<Integer> order, int R, int C, int[][] matrix) {
+        marked[curr] = true;
+
+        if (curr == R*C) {
+            order.push(curr);
+            for (int i = 0; i < R*C; i++) {
+                if (!marked[i]) {
+                    dfs(i, marked, order, R, C, matrix);
+                }
+                
+            }
+            marked[curr] = true;
+            return;
+        }
+
+        if (curr == R*C+1) {
+            order.push(curr);
+            marked[curr] = true;
+            return;
+        }
+
+        int r = curr / C;
+        int c = curr % C;
+
+        for (int d = 0; d < dirs.length; d++) {
+            int newR = r + dirs[d][0];
+            int newC = c + dirs[d][1];
+            if (newR >= 0 && newR < R && newC >= 0 && newC < C) {
+                int newI = newR*C + newC;
+                if (!marked[newI] && matrix[newR][newC] > matrix[r][c]) {
+                    dfs(newI, marked, order, R, C, matrix);
+                }
+            }
+        }
+        marked[curr] = true;
+        order.push(curr);
+    }
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
+        int[][] matrix = {
+            {9,9,4},
+            {6,6,8},
+            {2,1,1}
+        };
+        int expectedOutput = 4;
+        Solution sol = new Solution();
+        int output = sol.longestIncreasingPath(matrix);
+        boolean passed = output == expectedOutput;
+        System.out.println("test1: " + (passed ? "passed" : "failed"));
     }
 }
