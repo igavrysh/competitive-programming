dfb998020ab60d60dbeba5c77efb8b5ae28ab69b
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-12-29 22:53:54 -0800

    220 leetcode solution

--- algo/leetcode/0220-contains-duplicate-iii/SolutionBuckets.java
+++ algo/leetcode/0220-contains-duplicate-iii/SolutionBuckets.java
@@ -5,7 +5,7 @@ public class SolutionBuckets {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
         Map<Long, Long> buckets = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            long val = (long) nums[i] - Integer.MIN_VALUE;
+            long val = (long)nums[i] - Integer.MIN_VALUE;
             long bucket = val / ((long) valueDiff + 1);
             if(buckets.containsKey(bucket)
                 || (buckets.containsKey(bucket - 1) && val - buckets.get(bucket - 1) <= valueDiff)
